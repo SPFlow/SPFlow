d6ac97ee0626e3f9eb264c2ceb06037d11be20da
molina <molina@cs.tu-darmstadt.de>
2018-03-20 19:04:05 +0100

    validity

--- src/spn/algorithms/Validity.py
+++ src/spn/algorithms/Validity.py
@@ -3,13 +3,50 @@ Created on March 20, 2018
 
 @author: Alejandro Molina
 '''
+from src.spn.structure.Base import Sum, Leaf, Product
 
 
-def is_decomposable(node):
-    pass
+def is_consistent(node):
+    '''
+    all children of a product node have different scope
+    '''
+
+    if isinstance(node, Leaf):
+        return True
+
+    if isinstance(node, Product):
+        nscope = set(node.scope)
+
+        allchildscope = set()
+        sum_features = 0
+        for child in self.children:
+            sum_features += len(child.scope)
+            allchildscope = allchildscope | set(child.scope)
+
+        if allchildscope != set(nscope) or sum_features != len(allchildscope):
+            return False
+
+
+    return all(map(is_consistent, node.children))
+
 
 def is_complete(node):
-    pass
+    '''
+    all children of a sum node have same scope as the parent
+    '''
+
+    if isinstance(node, Leaf):
+        return True
+
+    if isinstance(node, Sum):
+        nscope = set(node.scope)
+
+        for child in node.children:
+            if nscope != set(child.scope):
+                return False
+
+    return all(map(is_complete, node.children))
+
 
 def is_valid(node):
-    pass
+    return is_consistent(node) and is_valid(node)
