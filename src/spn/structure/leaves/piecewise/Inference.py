a08a467e3e6d4cf63ec3af6bae99df7e381c16e6
MoritzKulessa <33871189+MoritzKulessa@users.noreply.github.com>
2018-05-22 19:59:22 +0200

    Removed method with range
    Former-commit-id: 7776b1058dcf9c3ad3618e4d44c121e525fe63cf

--- src/spn/structure/leaves/piecewise/Inference.py
+++ src/spn/structure/leaves/piecewise/Inference.py
@@ -28,51 +28,6 @@ def piecewise_log_likelihood(node, data, dtype=np.float64, context=None, node_lo
 
     return probs
 
-
-
-def piecewise_log_likelihood_range(node, ranges, dtype=np.float64, context=None, node_log_likelihood=None):
-    '''
-    Returns the probability for the given ranges.
-    
-    ranges is multi-dimensional array:
-    - First index specifies the instance
-    - Second index specifies the feature
-    
-    Each entry of range contains a Range-object or None (e.g. for piecewise-node NumericRange exists).
-    If the entry is None, then the log-likelihood probability of 0 will be returned.
-    '''
-    
-    #Assert context is not None and assert that the given node is only build on one instance
-    assert context is not None, "context is not none"
-    assert len(node.scope) == 1, node.scope
-    
-    #Initialize the return variable log_probs with zeros
-    log_probs = np.zeros((ranges.shape[0], 1), dtype=dtype)
-    
-    #Only select the ranges for the specific feature
-    ranges = ranges[:, node.scope[0]]
-    
-    
-    for i, rang in enumerate(ranges):
-        
-        #Skip if no range is specified aka use a log-probability of 0 for that instance
-        if rang is None:
-            continue
-        
-        #Skip if no values for the range are provided
-        if rang.is_impossible():
-            log_probs[i] = LOG_ZERO
-        
-        #Compute the sum of the probability of all possible values
-        p_sum = sum([_compute_probability_for_range(node, interval) for interval in rang.get_ranges()])
-            
-        if p_sum == 0:
-            log_probs[i] = LOG_ZERO
-        else:
-            log_probs[i] = np.log(p_sum)
-        
-    return log_probs
-
     
     
 def _compute_probability_for_range(node, interval):
