7bbe162d467853f1498f2863aeaf1a6f219ebeee
MoritzKulessa <33871189+MoritzKulessa@users.noreply.github.com>
2018-05-22 19:58:38 +0200

    Removed method with range
    Former-commit-id: 1cfbbda29d516439f01debe0598af9ef9058c34a

--- src/spn/structure/leaves/parametric/Inference.py
+++ src/spn/structure/leaves/parametric/Inference.py
@@ -79,54 +79,6 @@ def parametric_log_likelihood(node, data, dtype=np.float64, context=None, node_l
         raise Exception("Unknown parametric " + str(type(node)))
 
     return log_probs
-
-
-
-def parametric_log_likelihood_range(node, ranges, dtype=np.float64, context=None, node_log_likelihood=None):    
-    '''
-    Returns the probability for the given ranges.
-    
-    ranges is multi-dimensional array:
-    - First index specifies the instance
-    - Second index specifies the feature
-    
-    Each entry of range contains a Range-object or None (e.g. for categorical-node NominalRange exists).
-    If the entry is None, then the log-likelihood probability of 0 will be returned.
-    '''
-    
-    #Assert that the given node is only build on one instance
-    assert len(node.scope) == 1, node.scope
-    
-    #Initialize the return variable log_probs with zeros
-    log_probs = np.zeros((ranges.shape[0], 1), dtype=dtype)
-    
-    #Only select the ranges for the specific feature
-    ranges = ranges[:, node.scope[0]]
-    
-    #In case that the parametric node is categorical
-    if isinstance(node, Categorical):
-        
-        for i, rang in enumerate(ranges):
-            
-            #Skip if no range is specified aka use a log-probability of 0 for that instance
-            if rang is None:
-                continue
-            
-            #Skip if no values for the range are provided
-            if rang.is_impossible():
-                log_probs[i] = LOG_ZERO
-            
-            #Compute the sum of the probability of all possible values
-            p_sum = sum([node.p[possible_val] for possible_val in rang.get_ranges()])
-                
-            if p_sum == 0:
-                log_probs[i] = LOG_ZERO
-            else:
-                log_probs[i] = np.log(p_sum)
-                
-        return log_probs
-    else:
-        raise Exception("Unknown parametric for log likelihood ranges: " + str(type(node)))
     
 
 
