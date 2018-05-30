2cb3721e4578cac5a731d3cee46d649ea409067d
molina <molina@cs.tu-darmstadt.de>
2018-05-30 12:04:55 +0200

    Expectations
    Former-commit-id: 7832e70549f80941e57f0f5bd3a9140d5bf6ebbf

--- src/spn/algorithms/stats/Expectations.py
+++ src/spn/algorithms/stats/Expectations.py
@@ -1,14 +1,13 @@
 from spn.algorithms.Inference import log_likelihood
+from spn.algorithms.Marginalization import marginalize
 from spn.io.Text import spn_to_str_equation
 from spn.structure.Base import Leaf, Context
 from spn.structure.StatisticalTypes import MetaType
-from spn.structure.leaves.histogram.Histograms import add_domains, create_histogram_leaf
+from spn.structure.leaves.parametric.Expectation import add_parametric_expectation_support
 from spn.structure.leaves.parametric.Parametric import Gaussian
 from spn.structure.leaves.parametric.Text import add_parametric_text_support
 import numpy as np
 
-from spn.structure.leaves.piecewise.Expectation import piecewise_expectation
-from spn.structure.leaves.piecewise.PiecewiseLinear import create_piecewise_leaf
 
 _node_expectation = {}
 
@@ -16,18 +15,24 @@ _node_expectation = {}
 def add_node_expectation(node_type, lambda_func):
     _node_expectation[node_type] = lambda_func
 
-def Expectation(spn, feature_scope, evidence, ds_context, node_expectation=_node_expectation):
+
+def Expectation(spn, feature_scope, evidence_scope, evidence, ds_context, node_expectation=_node_expectation):
+    if evidence_scope is None:
+        evidence_scope = set()
+
+    mrag_spn = marginalize(spn, keep=feature_scope + evidence_scope)
+
     def leaf_expectation(node, data, dtype=np.float64, node_log_likelihood=None):
-        if node.scope[0] == feature_scope:
+        if node.scope[0] in feature_scope:
             t_node = type(node)
             if t_node in node_expectation:
-                return node_expectation[type(node)](node, ds_context)
+                return node_expectation[t_node](node, ds_context)
             else:
                 raise Exception('Node type unknown: ' + str(t_node))
 
         return log_likelihood(node, evidence)
 
-    log_expectation = log_likelihood(spn, evidence, node_log_likelihood={Leaf: leaf_expectation})
+    log_expectation = log_likelihood(mrag_spn, evidence, node_log_likelihood={Leaf: leaf_expectation})
 
     return np.exp(log_expectation)
 
@@ -35,20 +40,31 @@ def Expectation(spn, feature_scope, evidence, ds_context, node_expectation=_node
 if __name__ == '__main__':
     add_parametric_text_support()
 
+    add_parametric_expectation_support()
+
     spn = 0.3 * (Gaussian(1.0, 1.0, scope=[0]) * Gaussian(5.0, 1.0, scope=[1])) + \
           0.7 * (Gaussian(10.0, 1.0, scope=[0]) * Gaussian(15.0, 1.0, scope=[1]))
 
     print(spn_to_str_equation(spn))
 
-    data = np.array([0, 0, 1, 3]).reshape(-1, 1)
-    data = np.random.rand(5000).reshape(-1, 1)
+    evidence = np.random.rand(5000).reshape(-1, 2)
+    ds_context = Context(meta_types=[MetaType.REAL, MetaType.REAL])
+    ds_context.add_domains(evidence)
 
-    ds_context = Context(meta_types=[MetaType.DISCRETE])
-    ds_context = Context(meta_types=[MetaType.REAL])
-    add_domains(data, ds_context)
+    expectation = Expectation(spn, set([0]), None, evidence, ds_context)
 
-    node = create_piecewise_leaf(data, ds_context, scope=[0], prior_weight=None)
+    print("DISCRETE", "mean should be ", np.mean(data), "is", expectation)
 
-    exp = piecewise_expectation(node)
+    # data = np.array([0, 0, 1, 3]).reshape(-1, 1)
+    # data = np.random.rand(5000).reshape(-1, 1)
+    # data = np.random.randint(0, 20, size=10).reshape(-1, 1)
+    #
+    # ds_context = Context(meta_types=[MetaType.DISCRETE])
+    # # ds_context = Context(meta_types=[MetaType.REAL])
+    # add_domains(data, ds_context)
+    #
+    # node = create_piecewise_leaf(data, ds_context, scope=[0], prior_weight=None)
+    #
+    # exp = piecewise_expectation(node)
 
-    print("DISCRETE", "mean should be ", np.mean(data), "is", exp)
+
