c5e4ccf4ac849dd77c31b1df770d5627a96d0a1c
Max <maximilian.gottschalk94@gmail.com>
2018-07-03 16:23:20 +0200

    hyperspectral init
    Former-commit-id: 8804a661a4c14c77407db7f162ba9b625382c9f5

---
+++ src/spn/experiments/hyperspectral/DataManager.py
@@ -0,0 +1,106 @@
+import numpy as np
+import scipy.io as sio
+
+number_of_classes = 2
+
+
+# padding_mode = {'reflect', 'mean'}
+def get_data_in_window(window_size=3, features=None, padding_mode='reflect', three_classes=True):
+    if window_size % 2 == 0:
+        raise ValueError("Window size must be odd number but was {}!".format(window_size))
+    if features is None:
+        features = [20, 120]
+
+    offset = int(window_size / 2)
+
+    X, Y = read_img()
+    Y = Y - 1
+    data_size = X.shape[0] * X.shape[1]
+
+    X_padded = np.pad(X, int(window_size / 2), padding_mode)
+    Y_padded = np.pad(Y, int(window_size / 2), padding_mode)
+
+    num_classes = window_size * window_size
+    data = np.ndarray((data_size, num_classes + len(features) * num_classes))
+
+    counter_index = 0
+    for i, row in enumerate(Y_padded[offset:Y_padded.shape[0] - offset], start=offset):
+        for j, column in enumerate(row[offset:Y_padded.shape[1] - offset], start=offset):
+            img_data = list()
+            class_data = list()
+            # slinding window
+            for k in range(j - offset, j + offset + 1):
+                for l in range(i - offset, i + offset + 1):
+                    # extract features
+                    if three_classes:
+                        class_data.append(Y_padded[l][k])
+                    else:
+                        class_data.append(0.0 if Y_padded[l][k] == 2.0 else Y_padded[l][k])
+                    for m in features:
+                        img_data.append(X_padded[l][k][m])
+            data[counter_index][:] = np.append(img_data, class_data)
+            counter_index += 1
+
+    global number_of_classes
+    number_of_classes = len(np.unique(data[:, data.shape[1] - window_size ** 2:data.shape[1]]))
+
+    return data
+
+
+def get_number_of_classes():
+    return number_of_classes
+
+
+def get_data(size=300, values=None):
+    if values is None:
+        values = [20, 120]
+
+    vocab = {1.0: 'healthy', 2.0: 'diseased', 3.0: 'stem'}
+    X, Y = read_img()
+    counter_healthy = 0
+    counter_disease = 0
+    counter_stem = 0
+    counter_data = 0
+    data = np.ndarray((3 * size, len(values) + 1))
+    for i, row in enumerate(Y):
+        for j, col in enumerate(row):
+            if vocab[col] == 'healthy' and counter_healthy < size:
+                hyp_value = np.array(X[i, j][values])
+                data[counter_data] = np.append(hyp_value, [int(col - 1)])
+                counter_data += 1
+                counter_healthy += 1
+            elif vocab[col] == 'diseased' and counter_disease < size:
+                hyp_value = np.array(X[i, j][values])
+                data[counter_data] = np.append(hyp_value, [int(col - 1)])
+                counter_data += 1
+                counter_disease += 1
+            elif vocab[col] == 'stem' and counter_stem < size:
+                hyp_value = np.array(X[i, j][values])
+                data[counter_data] = np.append(hyp_value, [int(col - 1)])
+                counter_data += 1
+                counter_stem += 1
+            if counter_data >= 3 * size:
+                return data
+    raise ValueError("Not enough values in image (size: {})".format(size))
+
+
+def read_img(src="cerc15dai175.mat"):
+
+    try:
+        data = np.load("test.npz")
+        return data["X"], data["Y"]
+    except FileNotFoundError:
+        data = sio.loadmat(src)
+
+        dim = data["dim"].reshape(-1)
+        input_image = data["counts"]
+        input_image = input_image.T
+        input_image = np.reshape(input_image, newshape=[dim[0], dim[1], dim[2]])
+
+        input_image = input_image.astype(float)
+        input_image -= np.min(input_image)
+        input_image /= np.max(input_image)
+
+        np.savez("{}.npz".format(src.split(".")[0]), X=input_image, Y=data["labels"].reshape(dim[0], dim[1]))
+
+        return input_image, data["labels"].reshape(dim[0], dim[1])
---
+++ src/spn/experiments/hyperspectral/hyperspectral.py
@@ -0,0 +1,204 @@
+import numpy as np
+import scipy.io as sio
+import sklearn as sk
+import matplotlib.pyplot as plt
+import pickle
+from tqdm import tqdm
+
+from spn.algorithms.Inference import log_likelihood
+from spn.algorithms.LearningWrappers import learn_parametric
+from spn.experiments.hyperspectral.DataManager import *
+from spn.io.Text import spn_to_str_equation
+from spn.structure.Base import Context, Sum, assign_ids
+from spn.structure.StatisticalTypes import MetaType
+from spn.structure.leaves.parametric.Inference import add_parametric_inference_support, Gaussian
+from spn.structure.leaves.parametric.Parametric import Categorical
+from spn.structure.leaves.parametric.Text import add_parametric_text_support
+from spn.algorithms.Statistics import *
+
+
+def train_spn(window_size=5, min_instances_slice=10000, features=None):
+    if features is None:
+        features = [20, 120]
+
+    add_parametric_inference_support()
+    add_parametric_text_support()
+
+    data = get_data_in_window(window_size=window_size, features=features)
+
+    sss = sk.model_selection.StratifiedShuffleSplit(test_size=0.2, train_size=0.8, random_state=42)
+    for train_index, test_index in sss.split(data[:, 0:window_size * window_size * len(features)],
+                                             data[:, (window_size * window_size * len(features)) + (
+                                                     int(window_size * window_size / 2))]):
+        X_train, X_test = data[train_index], data[test_index]
+
+    context_list = list()
+    probability_list = list()
+    number_of_features = len(features)
+    for _ in range(number_of_features * window_size * window_size):
+        context_list.append(MetaType.REAL)
+        probability_list.append(Gaussian)
+
+    for _ in range(window_size * window_size):
+        context_list.append(MetaType.DISCRETE)
+        probability_list.append(Categorical)
+
+    ds_context = Context(meta_types=context_list)
+    ds_context.add_domains(data)
+    ds_context.parametric_type = probability_list
+
+    try:
+        if number_of_classes == 2:
+            spn = pickle.load(open("trainedSPNs/spn_{}_{}_{}.p".format(min_instances_slice, window_size, features), "rb"))
+        else:
+            spn = pickle.load(open("trainedSPNs/spn_all.p".format(min_instances_slice, window_size, features), "rb"))
+        print("Pretrained SPN (min_instances_slice={}, window_size={}) used".format(min_instances_slice, window_size))
+    except FileNotFoundError:
+        spn = Sum()
+        for class_pixel in tqdm(range(-window_size * window_size, 0)):
+            for label, count in zip(*np.unique(data[:, class_pixel], return_counts=True)):
+                train_data = X_train[X_train[:, class_pixel] == label, :]
+                branch = learn_parametric(train_data, ds_context, min_instances_slice=min_instances_slice)
+                spn.children.append(branch)
+                spn.weights.append(train_data.shape[0])
+
+        spn.scope.extend(branch.scope)
+        spn.weights = (np.array(spn.weights) / sum(spn.weights)).tolist()
+
+        assign_ids(spn)
+        if number_of_classes == 2:
+            pickle.dump(spn, open("trainedSPNs/spn_{}_{}_{}.p".format(min_instances_slice, window_size, features), "wb"))
+        else:
+            pickle.dump(spn, open("trainedSPNs/spn_all.p".format(min_instances_slice, window_size, features), "wb"))
+
+        print("New SPN (min_instances_slice={}, window_size={}) trained".format(min_instances_slice, window_size))
+
+    res = np.ndarray((X_test.shape[0], number_of_classes))
+
+    for i in tqdm(range(number_of_classes)):
+        tmp = X_test.copy()
+        tmp[:, -int((window_size ** 2) / 2)] = i
+        res[:, i] = log_likelihood(spn, tmp)[:, 0]
+
+    predicted_classes = np.argmax(res, axis=1).reshape((X_test.shape[0], 1))
+
+    correct_predicted = 0
+    for x, y in zip(X_test[:, -5], predicted_classes):
+        if x == y[0]:
+            correct_predicted += 1
+    accuracy = correct_predicted / X_test.shape[0]
+    return spn, accuracy
+
+    # print(spn)
+    # print(spn_to_str_equation(spn))
+    # print(log_likelihood(spn, data))
+
+
+def predict_img(spn, values=None, window_size=3):
+    if values is None:
+        values = [20, 140]
+
+    plt.subplot(1, 2, 1)
+    X, Y = read_img()
+    plt.title("Truth")
+    plt.imshow(Y)
+
+    def predict(spn):
+        data = get_data_in_window(features=values)
+        res = np.ndarray((Y.shape[0] * Y.shape[1], number_of_classes))
+        for i in range(number_of_classes):
+            tmp = data.copy()
+            tmp[:, -int((window_size ** 2) / 2)] = i
+            res[:, i] = log_likelihood(spn, tmp)[:, 0]
+        return np.argmax(res, axis=1).reshape((Y.shape[0], Y.shape[1]))
+
+    plt.subplot(1, 2, 2)
+    plt.title("Predicted")
+    a = predict(spn)
+    plt.imshow(a)
+
+    plt.savefig("comparison.png")
+    plt.show()
+
+
+def plot_experiments():
+    # # Plot different window size
+    # acc = list()
+    # x = list()
+    # for window_size in range(3,7,2):
+    #     x.append(window_size)
+    #     tmp = list()
+    #     for min_instances_slice in range (1,4):
+    #         _, accuracy = train_spn(window_size, 10**(min_instances_slice+1))
+    #         tmp.append(accuracy)
+    #     acc.append(tmp)
+    # plt.plot(x, acc, "x-")
+    # plt.title("Experiment with Window Size")
+    # plt.ylabel("Accuracy")
+    # plt.xlabel("Window Size")
+    # plt.savefig("exp_window_size.png")
+    # plt.show()
+
+    # Plot different window size
+    # acc = list()
+    # x = list()
+    # for window_size in range(3, 13, 2):
+    #     x.append(window_size)
+    #     _, accuracy = train_spn(window_size, 10000)
+    #     acc.append(accuracy)
+    # plt.plot(x, acc, "x-")
+    # plt.title("Experiment with Window Size")
+    # plt.ylabel("Accuracy")
+    # plt.xlabel("Window Size")
+    # plt.savefig("exp_window_size.png")
+    # plt.show()
+
+    # # Plot with different features
+    # acc = list()
+    # x = list()
+    # for i in range(10, 90, 10):
+    #     x.append(len(list(range(i, 90, 10))))
+    #     _, accuracy = train_spn(window_size=3, min_instances_slice=1000,features=list(range(i, 100, 10)))
+    #     acc.append(accuracy)
+    # plt.plot(x, acc, "x-")
+    # plt.title("Experiment with Window Size")
+    # plt.ylabel("Accuracy")
+    # plt.xlabel("Window Size")
+    # plt.savefig("exp_window_size.png")
+    # plt.show()
+
+    # # Plot different feature number
+    # acc = list()
+    # x = list()
+    # for i in range(14):
+    #     x.append(10 * (i + 1))
+    #     _, accuracy = train_spn(300, list(range(20, 30 + (i * 10))))
+    #     acc.append(accuracy)
+    # plt.plot(x, acc, "x-")
+    # plt.title("Experiment with Feature Size")
+    # plt.ylabel("Accuracy")
+    # plt.xlabel("Number of Features")
+    # plt.savefig("exp_number_of_features.png")
+    # plt.show()
+
+    feature_list = list(range(0, 160, 1))
+    spn, acc = train_spn(3, 4000, feature_list)
+    print(get_structure_stats(spn))
+    print("Accuracy on spn: {}".format(acc))
+    # predict_img(spn, feature_list)
+
+
+def find_good_features():
+    res = list()
+    for i in range(0, 160):
+        _, acc = train_spn(window_size=3, min_instances_slice=10000, features=[i])
+        res.append([i, acc])
+    return res
+
+
+if __name__ == '__main__':
+    # _, acc = train_spn()
+    # print("Accuracy: {}".format(acc))
+    plot_experiments()
+    # res = find_good_features()
+    # pass
