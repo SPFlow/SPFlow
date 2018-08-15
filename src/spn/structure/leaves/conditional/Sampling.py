678dbe4c36f90ffb960f0cfa42934d7cb873996d
Alejandro Molina <molina@cs.tu-darmstadt.de>
2018-08-15 16:21:38 -0700

    some refactoring of cspn + sampling
    Former-commit-id: 01b16d9681a6318afa111479667821a9fed68228

--- src/spn/structure/leaves/conditional/Sampling.py
+++ src/spn/structure/leaves/conditional/Sampling.py
@@ -1,35 +1,30 @@
 from spn.algorithms.Sampling import add_node_sampling
-from spn.structure.leaves.conditional.Conditional import Conditional, Conditional_Bernoulli, Conditional_Gaussian, Conditional_Poisson
+from spn.structure.leaves.conditional.Conditional import Conditional, Conditional_Bernoulli, Conditional_Gaussian, \
+    Conditional_Poisson
 
 import numpy as np
 
 from spn.structure.leaves.conditional.utils import get_scipy_obj_params
 
 
-def sample_conditional_node(node, obs, rand_gen):   # n_samples -> obs
-    assert isinstance(node, Conditional)
-    assert len(obs) > 0
+def sample_conditional_node(node, n_samples, data, rand_gen):  # n_samples -> obs
+    assert len(data) > 0
 
-    X = None
-    if isinstance(node, Conditional_Bernoulli) or isinstance(node, Conditional_Gaussian) or isinstance(node, Conditional_Poisson):
-
-        scipy_obj, params = get_scipy_obj_params(node, obs)
-
-        assert len(params) == obs.shape[0]
-
-        # X = scipy_obj.rvs(size=n_samples, random_state=rand_gen, **params)
-        X = [scipy_obj.rvs(size=1, random_state=rand_gen, **param) for param in params]
-
-        assert X.shape[0] == obs.shape[0]
-
-    else:
+    if not isinstance(node, (Conditional_Bernoulli, Conditional_Gaussian, Conditional_Poisson)):
         raise Exception('Node type unknown: ' + str(type(node)))
 
+    scipy_obj, params = get_scipy_obj_params(node, data[:, node.evidence_size:])
+
+    X = scipy_obj.rvs(size=data.shape[0], random_state=rand_gen, **params)
+
+    assert X.shape[0] == data.shape[0]
+
     return X
+
+
 
 
 def add_conditional_sampling_support():
     add_node_sampling(Conditional_Bernoulli, sample_conditional_node)
     add_node_sampling(Conditional_Gaussian, sample_conditional_node)
     add_node_sampling(Conditional_Poisson, sample_conditional_node)
-
