d81b917e90c49001a4a08a222faaae62ca3936a2
xiaotingshao <640000828@qq.com>
2018-08-29 17:44:14 +0200

    glm with tensorflow
    Former-commit-id: 734085e4881cd38283cd4bfd3f14ce23766d6870

--- src/spn/structure/leaves/conditional/MLE.py
+++ src/spn/structure/leaves/conditional/MLE.py
@@ -4,12 +4,17 @@ Created on April 15, 2018
 @author: Alejandro Molina
 '''
 import numpy as np
+import warnings
 from scipy.stats import gamma, lognorm
+import tensorflow as tf
+import tensorflow_probability as tfp;  tfd = tfp.distributions
 
 from spn.structure.leaves.conditional.Conditional import Conditional_Gaussian, Conditional_Poisson, \
     Conditional_Bernoulli
 import statsmodels.api as sm
 
+from os.path import dirname
+path = dirname(__file__) + "/"
 
 def update_glm_parameters_mle(node, data, scope):  # assume data is tuple (output np array, conditional np array)
 
@@ -34,4 +39,27 @@ def update_glm_parameters_mle(node, data, scope):  # assume data is tuple (outpu
         raise Exception("Unknown conditional " + str(type(node)))
 
     dataIn = np.c_[dataIn, np.ones((dataIn.shape[0]))]
-    node.weights = sm.GLM(dataOut, dataIn, family=family).fit_regularized(alpha=0.0001).params
+    try:
+        dataOut = dataOut.reshape(-1)
+        w, linear_response, is_converged, num_iter = tfp.glm.fit(
+            model_matrix=tf.constant(dataIn),
+            response=tf.constant(dataOut),
+            model=tfp.glm.Poisson(),
+            l2_regularizer=0.0001)
+        log_likelihood = tfp.glm.Poisson().log_prob(tf.constant(dataOut), linear_response)
+
+        with tf.Session() as sess:
+            [w_, linear_response_, is_converged_, num_iter_, Y_, log_likelihood_] = sess.run([w, linear_response, is_converged, num_iter, tf.constant(dataOut), log_likelihood])
+
+        node.weights = w_
+        print("node.weights", node.weights)
+        # glmfit = sm.GLM(dataOut, dataIn, family=family).fit_regularized(alpha=0.001)
+        # node.weights = glmfit.params
+        # # if glmfit.converged is False:
+        # #     warnings.warn("Maximum number of iterations reached")
+    except Exception:
+        glmfit = sm.GLM(dataOut, dataIn, family=family).fit_regularized(alpha=0.0001)
+        node.weights = glmfit.params
+        print("node.weights with glmfit", node.weights)
+        np.savez(path+"tmp_glm_mle_data", dataIn=dataIn, dataOut=dataOut)
+
