80933766874358e8f8b4849974eb2398fdc3ed2e
Alejandro Molina <molina@cs.tu-darmstadt.de>
2018-08-31 16:39:21 +0200

    performance improvements
    Former-commit-id: e1cc4f2d32f105f2b2d285313e70f37ae4c693e0

--- src/spn/experiments/RelationalCutSet/LearnStructure.py
+++ src/spn/experiments/RelationalCutSet/LearnStructure.py
@@ -126,66 +126,6 @@ def load_tables(path, meta_data, debug=False):
     return tables
 
 
-def get_values(attributes_in_table, meta_data, tables, att_name, constraints, table_row_idxs=None):
-    result_set = None
-    value_count = {}
-    filtered_table_row_idxs_by_value = {}
-    for table_name in attributes_in_table[att_name]:
-        table_meta_data = meta_data[table_name]
-        att_pos = table_meta_data[att_name]
-        table = tables[table_name]
-
-        # this is optimization
-        row_mask = None
-        row_idx = None
-        if table_row_idxs is not None and table_name in table_row_idxs:
-            row_idx = table_row_idxs[table_name]
-            row_mask = row_idx > -1
-        if row_mask is None:
-            row_mask = np.ones(table.shape[0]) == 1
-            row_idx = np.array(list(range(table.shape[0])))
-
-        # this computes the constraints
-        for constraint_attribute, constraint_value in constraints.items():
-            constraint_att_pos = table_meta_data.get(constraint_attribute, None)
-            if constraint_att_pos is None:
-                continue
-            row_mask = row_mask & (table[row_idx, constraint_att_pos] == constraint_value)
-
-        # data contains the column with all the values we want, after filtering
-        data = table[row_idx[row_mask], att_pos]
-
-        # obtain unique values
-        unique, rev_idx, count = np.unique(data, return_counts=True, return_inverse=True)
-        for i, val in enumerate(unique):
-
-            # this is optimization, to not fully scan all tables always
-            table_row_idx_by_val = filtered_table_row_idxs_by_value.get(val, None)
-            if table_row_idx_by_val is None:
-                table_row_idx_by_val = {}
-                filtered_table_row_idxs_by_value[val] = table_row_idx_by_val
-            table_row_idx = table_row_idx_by_val.get(table_name, None)
-            if table_row_idx is None:
-                table_row_idx_by_val[table_name] = row_idx[rev_idx == i]
-
-            # how many instances were found on the join?
-            if val not in value_count:
-                value_count[val] = count[i]
-            else:
-                value_count[val] = max(value_count[val], count[i])
-
-        # we are only interested in the values that are intersected on all tables
-        table_values = set(unique)
-        if result_set is None:
-            result_set = table_values
-        else:
-            result_set.intersection_update(table_values)
-
-    # return the values, the counts, and the row indexes where those values are found
-    for val in result_set:
-        yield (val, value_count[val], filtered_table_row_idxs_by_value[val])
-
-
 def get_keys(dep_tree, meta_data, attributes_in_table):
     # keys are the attributes that show up in more than one table.
     keys = set()
@@ -235,10 +175,19 @@ def build_cache(tables, meta_data, table_keys, attribute_owners):
     cache = {}
     for table_name, constraint_groups in table_keys.items():
         table = tables[table_name]
+        print("loading: ", table_name)
+
+        table_cache = cache.get(table_name, None)
+        if table_cache is None:
+            table_cache = cache[table_name] = {}
 
         for keys in constraint_groups:  # for different clustering orders
             constraint_atts = None
 
+            print("loading constraint group: ", constraint_groups)
+            table_key = tuple(keys)
+
+            sort_order = []
             for att_name in keys:
                 att_pos = meta_data[table_name][att_name]
 
@@ -249,29 +198,31 @@ def build_cache(tables, meta_data, table_keys, attribute_owners):
                     constraint_atts = new_constraints
                 else:
                     constraint_atts = list(itertools.product(constraint_atts, new_constraints))
+                sort_order.append(table[:, meta_data[table_name][att_name]])
 
+            sorted_table = table[np.lexsort(sort_order), :].astype(float)
             for constraint in constraint_atts:
+                print(constraint)
                 # get data
-                idx = np.ones(table.shape[0]) == 1
                 if not isinstance(constraint[0], tuple):
                     constraint = [constraint]
 
                 for grounding in constraint:
-                    idx = idx & (table[:, meta_data[table_name][grounding[0]]] == grounding[1])
-                group_data = table[idx, :].astype(float)
+                    sorted_col = sorted_table[:, meta_data[table_name][grounding[0]]]
+                    l = np.searchsorted(sorted_col, grounding[1], side='left')
+                    r = np.searchsorted(sorted_col, grounding[1], side='right')
+                    if l == r:
+                        # constraint not found
+                        continue
+                    sorted_table = sorted_table[l:r, :]
+                group_data = sorted_table
 
                 if group_data.shape[0] == 0:
-                    print("WARNING: empty group data")
                     continue
 
-                # compute leaf
-                if table_name not in cache:
-                    cache[table_name] = {}
-                table_cache = cache[table_name]
-                table_key = tuple(keys)
-                if table_key not in table_cache:
-                    table_cache[table_key] = {}
-                constraint_cache = table_cache[table_key]
+                constraint_cache = table_cache.get(table_key, None)
+                if constraint_cache is None:
+                    constraint_cache = table_cache[table_key] = {}
 
                 p_node = Product()
                 for i, (c, val) in enumerate(constraint):
@@ -280,7 +231,7 @@ def build_cache(tables, meta_data, table_keys, attribute_owners):
                     constraint_cache = constraint_cache[c]
                     if val not in constraint_cache:
                         if i == len(constraint) - 1:
-                            constraint_cache[val] = (p_node, np.sum(idx))
+                            constraint_cache[val] = (p_node, group_data.shape[0])
                         else:
                             constraint_cache[val] = {}
                     constraint_cache = constraint_cache[val]
@@ -336,52 +287,6 @@ def get_table_ancestors(dep_tree, attributes_in_table):
             keys_to_tables[key].append(table_name)
 
     return table_keys, keys_to_tables, ancestors
-
-
-def build_csn(attributes_in_table, meta_data, tables, dependency_node, table_keys, ancestors, constraints=None,
-              table_row_idxs=None, cache=None,
-              debug=False):
-    if constraints is None:
-        constraints = {}
-
-    att_name = dependency_node.name
-
-    if att_name[0] == "@":
-        key = keys_per_attribute[att_name]
-        cache_key = tuple([(k, constraints[k]) for k in key if k in constraints])
-        return cache[att_name[1:]][cache_key]
-
-    new_node = Sum()
-
-    new_constraints = dict(constraints)
-
-    for val, count, filtered_table_row_idxs in get_values(attributes_in_table, meta_data, tables, att_name,
-                                                          constraints, table_row_idxs):
-        if debug:
-            print("att_name", att_name, val)
-        new_node.weights.append(count)
-
-        p_node = Product()
-        p_node.children.append(CategoricalDictionary(p={float(val): 1.0}, scope=scopes[att_name]))
-
-        for dep_node in dependency_node.children:
-            new_constraints[att_name] = val
-            p_node.children.append(
-                build_csn(attributes_in_table, meta_data, tables, dep_node, table_keys, ancestors,
-                          constraints=new_constraints,
-                          table_row_idxs=filtered_table_row_idxs,
-                          cache=cache,
-                          debug=False))
-
-        new_node.children.append(p_node)
-
-    wsum = np.sum(new_node.weights)
-    new_node.weights = [w / wsum for w in new_node.weights]
-
-    if can_cache:
-        cache[cache_key] = new_node
-
-    return new_node
 
 
 def get_dependncy_keys(dep_tree, table_keys, path_constraints):
@@ -453,7 +358,7 @@ def build_csn2(dep_tree, table_keys, scopes, path_constraints=None, cache=None):
                 p_node.children.append(
                     build_csn2(dep_children_node, table_keys, scopes, path_constraints=new_constraints, cache=cache))
 
-        wsum = np.sum(new_node.weights)
+    wsum = np.sum(new_node.weights)
     new_node.weights = [w / wsum for w in new_node.weights]
 
     return new_node
@@ -482,18 +387,19 @@ if __name__ == '__main__':
 
     tables = load_tables(path, meta_data, debug=False)
 
-    cache = build_cache(tables, meta_data, table_keys, attribute_owners)
 
     spn = None
 
     file_cache_path = "/tmp/csn.bin"
-    if not os.path.isfile(file_cache_path) or True:
+    if not os.path.isfile(file_cache_path):
+        cache = build_cache(tables, meta_data, table_keys, attribute_owners)
+
         spn = build_csn2(dep_tree, table_keys, scopes, path_constraints=None, cache=cache)
         rebuild_scopes_bottom_up(spn)
         print(spn)
         assign_ids(spn)
-        print(is_valid(spn))
-        print(spn_to_str_ref_graph(spn))
+        # print(is_valid(spn))
+        # print(spn_to_str_ref_graph(spn))
 
         keep = set(scopes.values())
         keep.discard(scopes["userid"])
@@ -524,16 +430,16 @@ if __name__ == '__main__':
         # prob_str = "P(%s|%s)" % (query_str, evidence_str)
         # print("computing ", prob_str)
 
-        a = log_likelihood(spn, to_data(scopes, rows, **q_e), debug=False)
+        a = log_likelihood(spn, to_data(scopes, rows, **q_e), debug=True)
         # print("query ", query_str, np.exp(a))
-        b = log_likelihood(spn, to_data(scopes, rows, **evidence), debug=False)
+        b = log_likelihood(spn, to_data(scopes, rows, **evidence), debug=True)
         # print("evidence ", evidence_str, b, np.exp(b))
         result = np.exp(a - b)
         # print(prob_str, "=", result, "query ", query_str, np.exp(a), evidence_str, np.exp(b))
         return result
 
 
-    print(spn_to_str_ref_graph(spn))
+    #print(spn_to_str_ref_graph(spn))
 
     evidence_atts = ['year', 'action', 'adventure', 'animation', 'children', 'comedy', 'crime', 'documentary', 'drama',
                      'fantasy', 'filmnoir', 'horror', 'musical', 'mystery', 'romance', 'scifi',
@@ -554,7 +460,7 @@ if __name__ == '__main__':
 
     for i in range(4):
         sdiff = np.sum(np.abs(true_ratings - pred_ratings) <= i)
-        print("rating diffeerence at most", i, sdiff, sdiff/test_table.shape[0])
+        print("rating diffeerence at most", i, sdiff, sdiff / test_table.shape[0])
 
     from sklearn.metrics import mean_squared_error
     from math import sqrt
