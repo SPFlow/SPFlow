9cb822feee6ec121ba64b3edff0f66f37ee54f32
cvoelcker (iliricon) <claas@voelcker.net>
2018-09-24 17:44:57 +0200

    Adds gradient forward propagation algorithm
    Former-commit-id: 56cc582621913cfd801251aacb11d274b243b313

--- src/spn/algorithms/__init__.py
+++ src/spn/algorithms/__init__.py
@@ -1,14 +1,19 @@
 from spn.algorithms.MPE import add_node_mpe, mpe_leaf
+
 from spn.structure.leaves.histogram.Expectation import add_histogram_expectation_support
 from spn.structure.leaves.histogram.Inference import add_histogram_inference_support
 from spn.structure.leaves.histogram.MPE import add_histogram_mpe_support
+from spn.structure.leaves.histogram.Gradients import add_histogram_gradient_support
+
 from spn.structure.leaves.parametric.Expectation import add_parametric_expectation_support
 from spn.structure.leaves.parametric.Inference import add_parametric_inference_support
 from spn.structure.leaves.parametric.MPE import add_parametric_mpe_support
 from spn.structure.leaves.parametric.Sampling import add_parametric_sampling_support
+
 from spn.structure.leaves.piecewise.Expectation import add_piecewise_expectation_support
 from spn.structure.leaves.piecewise.Inference import add_piecewise_inference_support
 from spn.structure.leaves.piecewise.MPE import add_piecewise_mpe_support
+from spn.structure.leaves.piecewise.Gradients import add_piecewise_linear_gradient_support
 
 add_parametric_sampling_support()
 add_parametric_inference_support()
@@ -18,7 +23,9 @@ add_parametric_mpe_support()
 add_piecewise_inference_support()
 add_piecewise_expectation_support()
 add_piecewise_mpe_support()
+add_piecewise_linear_gradient_support()
 
 add_histogram_inference_support()
 add_histogram_expectation_support()
 add_histogram_mpe_support()
+add_histogram_gradient_support()
