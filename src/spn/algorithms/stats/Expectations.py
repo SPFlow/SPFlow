d2bf55bc38ac97ab29ddead0fc0776d41c5ec912
cvoelcker (iliricon) <claas@voelcker.net>
2018-09-25 18:51:17 +0200

    WiP Buggy means code
    Former-commit-id: c0ef8076e93c5a7b719fb31e5fe49af16c4e945c

--- src/spn/algorithms/stats/Expectations.py
+++ src/spn/algorithms/stats/Expectations.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from spn.algorithms.Inference import likelihood
 from spn.algorithms.Marginalization import marginalize
-from spn.structure.Base import Leaf, get_nodes_by_type, Sum, Product, set_full_scope
+from spn.structure.Base import Leaf, get_nodes_by_type, Sum, Product, set_full_scope, eval_spn_bottom_up
 
 _node_expectation = {}
 
@@ -12,20 +12,15 @@ def add_node_expectation(node_type, lambda_func):
 
 
 def prod_expectation(node, children, input_vals, moment=1, dtype=np.float64):
-    assert node.full_scope is not None, 'Scope not set'
-    llchildren = np.array(children).reshape(len(children), len(node.full_scope))
+    llchildren = np.concatenate(children, axis=1)
     ret_val = np.nansum(llchildren, axis=0, keepdims=True)
-    assert len(ret_val.shape) == 2, 'Wrong return dimensionality'
-    assert ret_val.shape[1] == len(node.full_scope), 'Wrong length of return array'
     return ret_val
 
 
 def sum_expectation(node, children, input_vals, moment=1, dtype=np.float64):
-    assert node.full_scope is not None, 'Scope not set'
-    llchildren = np.array(children).reshape(len(children), len(node.full_scope))
+    llchildren = np.array(children).reshape(len(children), len(node.scope))
     b = np.array(node.weights, dtype=dtype).reshape(1, -1)
     ret_val = np.dot(b, llchildren)
-    assert len(ret_val.shape) == 2, 'Wrong return dimensionality'
     return ret_val
 
 
@@ -43,9 +38,6 @@ def Expectation(spn, feature_scope, evidence_scope, evidence, node_expectation=_
     if evidence_scope is None:
         evidence_scope = set()
 
-    if not spn.full_scope:
-        set_full_scope(spn)
-
     assert not (len(evidence_scope) > 0 and evidence is None)
 
     assert len(feature_scope.intersection(evidence_scope)) == 0
@@ -53,31 +45,19 @@ def Expectation(spn, feature_scope, evidence_scope, evidence, node_expectation=_
     marg_spn = marginalize(spn, keep=feature_scope | evidence_scope)
     set_full_scope(marg_spn)
 
-    def leaf_expectation(node, data, dtype=np.float64, **kwargs):
-        if node.scope[0] in feature_scope:
-            t_node = type(node)
-            if t_node in node_expectation:
-                exps = np.zeros((data.shape[0], data.shape[1]), dtype=dtype)
-                exps[:] = node_expectation[t_node](node, moment=moment)
-                return exps
-            else:
-                raise Exception('Node type unknown: ' + str(t_node))
-
-        return likelihood(node, evidence)
-
-    node_expectations = {type(leaf): leaf_expectation for leaf in get_nodes_by_type(marg_spn, Leaf)}
-    node_expectations.update({Sum: lambda x, c, input, dtype: sum_expectation(x, c, input, moment=moment, dtype=dtype),
-                              Product: lambda x, c, input, dtype: prod_expectation(x, c, input, moment=moment, dtype=dtype)})
+    node_expectations = _node_expectation
+    node_expectations.update({Sum: sum_expectation,
+                              Product: prod_expectation})
 
     if evidence is None:
         # fake_evidence is not used
         fake_evidence = np.zeros((1, len(spn.scope))).reshape(1,-1)
-        expectation = likelihood(marg_spn, fake_evidence, node_likelihood=node_expectations)
+        expectation = eval_spn_bottom_up(marg_spn, node_expectations, moment=moment)
         return expectation
 
     # if we have evidence, we want to compute the conditional expectation
-    expectation = likelihood(marg_spn, evidence, node_likelihood=node_expectations)
-    expectation = expectation / likelihood(marginalize(marg_spn, keep=evidence_scope), evidence)
+    else:
+        raise NotImplementedError('Please use a conditional SPN to calculated conditional expectations')
 
     return expectation
 
