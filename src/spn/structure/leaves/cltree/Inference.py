aa7ee434bb06d26373a01989b8fb7bbb83b69819
Nicola Di Mauro <nicola.dimauro@uniba.it>
2018-10-23 15:05:20 +0200

    completed cltree inference and learning
    Former-commit-id: b72d502f8f93fbe92f19aa48e67779999610d8ed

--- src/spn/structure/leaves/cltree/Inference.py
+++ src/spn/structure/leaves/cltree/Inference.py
@@ -5,9 +5,23 @@ Created on October 19, 2018
 '''
 
 from spn.algorithms.Inference import add_node_likelihood
+from spn.structure.leaves.cltree.CLTree import CLTree
+
+import numpy as np
 
 def cltree_likelihood(node, data=None, dtype=np.float64):
-    raise ValueError('Not Implemented')
+    probs = np.zeros(data.shape[0], dtype=dtype)
+
+    for feature in range(0, node.n_features):
+        parent = node.tree[feature]
+        if parent == -1:
+            probs = probs + node.log_factors[feature, data[:,node.scope[feature]],0]
+        else:
+            probs = probs + node.log_factors[feature, data[:,node.scope[feature]], data[:,node.scope[parent]]]
+
+    return np.exp(probs.reshape(data.shape[0],1))
+    
+
 
 def add_cltree_inference_support():
     add_node_likelihood(CLTree, cltree_likelihood)
