fe30cc5474077c71f4a0e0fe2971b5f07126aabf
Nicola Di Mauro <nicola.dimauro@uniba.it>
2018-10-31 12:28:01 +0100

    refactoring
    Former-commit-id: 40e3553065bc479cf606f22ab688045662db74ab

--- src/spn/structure/leaves/cltree/CLTree.py
+++ src/spn/structure/leaves/cltree/CLTree.py
@@ -7,9 +7,13 @@ from spn.structure.Base import Leaf
 from spn.structure.StatisticalTypes import Type
 
 class CLTree(Leaf):
-    def __init__(self, scope=None):
+    def __init__(self, scope=None, data=None):
         self._type = Type.BINARY
         Leaf.__init__(self, scope=scope)
+
+        assert data is not None
+        
+        self.n_features = data.shape[1]
 
     @property
     def type(self):
@@ -23,7 +27,7 @@ class CLTree(Leaf):
 def create_cltree_leaf(data, ds_context, scope):
     from spn.structure.leaves.cltree.MLE import update_cltree_parameters_mle
     
-    node = CLTree(scope)
+    node = CLTree(scope, data)
     update_cltree_parameters_mle(node, data)
     
     return node
