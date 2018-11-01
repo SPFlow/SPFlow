1097e31ed1674126f7517234918d22ea449ef820
cvoelcker (iliricon) <claas@voelcker.net>
2018-11-01 19:21:47 +0100

    Added test conforming behavior
    Former-commit-id: efe95a661e84ef0309f545c902b633879f075ec5

--- src/spn/structure/leaves/piecewise/Moment.py
+++ src/spn/structure/leaves/piecewise/Moment.py
@@ -10,7 +10,7 @@ from spn.algorithms.stats.Moments import add_node_moment
 from spn.structure.leaves.piecewise.PiecewiseLinear import PiecewiseLinear
 
 
-def piecewise_moment(node, oder=1):
+def piecewise_moment(node, order=1):
     exp = 0
     for i in range(len(node.x_range) - 1):
         y0 = node.y_range[i]
@@ -21,7 +21,7 @@ def piecewise_moment(node, oder=1):
         # compute the line of the top of the trapezoid
         m = (y0 - y1) / (x0 - x1)
         b = - m * x0 + y0
-        k = oder
+        k = order
         integral = m / (k + 2) * (x1 ** (k + 2) - x0 ** (k + 2)) + b / (k + 1) * (x1 ** (k + 1) - x0 ** (k + 1))
         exp += integral
     return np.array([[exp]])
