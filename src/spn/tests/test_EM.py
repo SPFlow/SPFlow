b582822ed796fb930c91672adc941d85a1523994
molina <molina@cs.tu-darmstadt.de>
2018-11-22 15:46:22 +0100

    em
    Former-commit-id: 717f6d29e166c7826a67b07899974b209518d47d

--- src/spn/tests/test_EM.py
+++ src/spn/tests/test_EM.py
@@ -18,7 +18,7 @@ class TestEM(unittest.TestCase):
 
     def test_optimization(self):
         np.random.seed(17)
-        data = np.random.normal(10, 0.01, size=2000).tolist() + np.random.normal(30, 10, size=2000).tolist()
+        data = np.random.normal(10, 5, size=2000).tolist() + np.random.normal(30, 10, size=2000).tolist()
         data = np.array(data).reshape((-1, 10))
         data = data.astype(np.float32)
 
@@ -27,17 +27,17 @@ class TestEM(unittest.TestCase):
         spn = learn_parametric(data, ds_context)
 
         spn.weights = [0.8, 0.2]
-        spn.children[0].mean = 1
+        spn.children[0].children[0].mean = 3.0
 
         py_ll = log_likelihood(spn, data)
 
         print(spn.weights)
-        print(spn.children[0].mean)
+        print(spn.children[0].children[0].mean)
 
         EM_optimization(spn, data)
 
         print(spn.weights)
-        print(spn.children[0].mean)
+        print(spn.children[0].children[0].mean)
 
         py_ll_opt = log_likelihood(spn, data)
 
