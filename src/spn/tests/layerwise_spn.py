63c5c2133ed1001150ca8772f2a3a30f77583b99
Pranav Subramani <pranav0212.s@gmail.com>
2018-11-30 19:27:55 -0500

    breaking branch temporarily

--- src/spn/tests/layerwise_spn.py
+++ src/spn/tests/layerwise_spn.py
@@ -1,82 +1,56 @@
-import unittest
-from spn.algorithms.Inference import log_likelihood
-from spn.algorithms.LearningWrappers import learn_parametric, learn_mspn
-from spn.structure.leaves.parametric.parametric import Gaussian
-from spn.structure.StatisticalTypes import MetaType
+import structure
 import numpy as np
-from spn.structure.Base import Context
-import numpy as np
-import torch
-import pdb
-import math
 from collections import defaultdict, deque
-
-# class TestPytorch(unittest.TestCase):
-
-
-def test_eval_gaussian():
-    np.random.seed(17)
-    data = np.random.normal(10, 0.01, size=2000).tolist() + \
-        np.random.normal(30, 10, size=2000).tolist()
-    data = np.array(data).reshape((-1, 10))
-    data = data.astype(np.float32)
-
-    ds_context = Context(meta_types=[MetaType.REAL] * data.shape[1],
-                         parametric_types=[Gaussian] * data.shape[1])
-    spn = learn_parametric(data, ds_context)
-    # ll = log_likelihood(spn, data)
-
-    # tf_ll = eval_pytorch(spn, data)
-    return spn
-    # self.assertTrue(np.all(np.isclose(ll, tf_ll)))
-
-
-spn = test_eval_gaussian()
 
 sum_type = "sum"
 prd_type = "prd"
 
 
 class CVMetaData(object):
-    def __init__(self, spn):
+    def __init__(self, cv):
         self.depth = 0
         self.masks_by_level = []
         self.type_by_level = []
+        self.connections_by_level = []
+        self.weight_indices_by_level = []
         self.level_label_by_node = {}
         self.num_nodes_by_level = []
         self.nodes_by_level = []
         # The input index of the i-th leaf.
         self.leaves_input_indices = []
-        self.get_cv_metadata(spn)
+        self.leaves_network_id = []
+        self.num_leaves_per_network = None
+        self.weights = cv.weights  # TODO: Make sure this is right
+        self.get_cv_metadata(cv)
 
-    def get_cv_metadata(self, spn):
+    def get_cv_metadata(self, cv):
         '''
         Returns a node labelling scheme.
         Perform a per level labelling to each node.
         Defines the order in which each node appears in the matrix.
         '''
         level = 0
-        q = deque([spn])
+        q = deque(cv.roots)
         visited = {}
         # Perform a per level traversal
         while q:
-            pdb.set_trace()
             level_size = len(q)
             curr_level = []  # nodes at the current level
             level_type = None
             for i in range(level_size):
                 node = q.popleft()
                 curr_level.append(node)
-                if isinstance(node, Leaf):
+                if isinstance(node, structure.leaf.Leaf):
                     continue
                 else:
-                    node_type = sum_type if isinstance(node, Sum) else prd_type
+                    node_type = sum_type if isinstance(node, structure.node.Sum) else prd_type
                     if level_type is None:
                         level_type = node_type
                     elif node_type != level_type:
                         error = "Level type mismatch: Expects " + level_type + " gets " + node_type
                         raise Exception(error)
-                for child in node.children:
+                for edge in node.edges:
+                    child = edge.child
                     if child in visited:
                         continue
                     visited[child] = True
@@ -93,6 +67,7 @@ class CVMetaData(object):
         self.masks_by_level = self.get_masks_by_level(cv)
         self.leaves_network_id = self.get_leaves_network_id(cv)
         self.leaves_input_indices = self.get_leaves_input_indices(cv)
+        (self.connections_by_level, self.weight_indices_by_level) = self.get_connections_and_weights_by_level(cv)
         self.num_leaves_per_network = cv.x_size * cv.y_size
 
     def get_masks_by_level(self, cv):
@@ -108,7 +83,8 @@ class CVMetaData(object):
             cur_level_nodes = self.nodes_by_level[cur_level]
             for cur_node in cur_level_nodes:
                 cur_label = self.level_label_by_node[cur_node]
-                for child_node in cur_node.children:
+                for child_edge in cur_node.edges:
+                    child_node = child_edge.child
                     child_label = self.level_label_by_node[child_node]
                     level_mask[cur_label][child_label] = 1
             level_mask = level_mask.T
@@ -131,6 +107,32 @@ class CVMetaData(object):
             leaves_network_id.append(int(leaf.network_id))
         return leaves_network_id
 
+    def get_connections_and_weights_by_level(self, cv):
+        '''
+        Returns TorchSPN style sparse connection corresponding to ConvSPN cv
+        '''
+        connections_by_level = []
+        weight_indices_by_level = []
+        for cur_level in range(self.depth - 1):
+            next_level = cur_level + 1
+            cur_level_count = self.num_nodes_by_level[cur_level]
+            next_level_count = self.num_nodes_by_level[next_level]
+            cur_level_nodes = self.nodes_by_level[cur_level]
+            level_connections = [[] for c in cur_level_nodes]
+            weights = [[] for c in cur_level_nodes]
+            level_type = cur_level_nodes[0].node_type
+            for cur_node in cur_level_nodes:
+                cur_label = self.level_label_by_node[cur_node]
+                for child_edge in cur_node.edges:
+                    child_node = child_edge.child
+                    child_label = self.level_label_by_node[child_node]
+                    level_connections[cur_label].append(child_label)
+                    if level_type == 'Sum':
+                        weights[cur_label].append(child_edge.weight_id)
+            connections_by_level.append(level_connections)
+            weight_indices_by_level.append(weights)
+        return (connections_by_level, weight_indices_by_level)
+
 
 def get_edge_count_from_layers(cv_layers):
     '''
@@ -142,4 +144,15 @@ def get_edge_count_from_layers(cv_layers):
     return edge_count
 
 
-cvm = CVMetaData(spn)
+total_edge_count = 0
+edges = []
+
+
+def get_edges(level, level_type, level_nodes, edge_count):
+    if level_type != 'Sum':
+        return
+    global edges, total_edge_count
+    for node in level_nodes:
+        level_edges = list(node.weight_id_by_child.values())
+        edges.extend(level_edges)
+    total_edge_count += edge_count
