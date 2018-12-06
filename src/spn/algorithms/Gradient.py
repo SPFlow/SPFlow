10a2ef19ee3851f2bd5aa6bc1b9d9cee3b03e5f4
cvoelcker (iliricon) <claas@voelcker.net>
2018-12-06 17:28:50 +0100

    Added feature gradients
    Gradients per feature can now be computed, conditional gradients are not
    tested yet. Old tests for gradients work again

--- src/spn/algorithms/Gradient.py
+++ src/spn/algorithms/Gradient.py
@@ -1,5 +1,11 @@
-from spn.structure.Base import eval_spn_top_down, Sum, Product, Leaf
+from spn.structure.Base import eval_spn_top_down, Sum, Product, Leaf, bfs, get_number_of_nodes
+from spn.algorithms.Inference import log_likelihood
+from spn.algorithms.Marginalization import marginalize
 import numpy as np
+import collections
+
+
+_node_gradient = {}
 
 
 def gradient_backward(spn, lls_per_node):
@@ -61,3 +67,67 @@ def prod_gradient_backward(node, parent_result, gradient_result=None, lls_per_no
     assert not np.any(np.isnan(messages_to_children)), "Nans found in iteration"
 
     return messages_to_children
+
+
+def add_node_gradient(node_type, lambda_func):
+    _node_gradient[node_type] = lambda_func
+
+
+def feature_gradient(node, data, node_gradient_functions=_node_gradient, gradient_result=None, lls_per_node=None):
+    '''
+    Feature gradients are computed for the input query and each feature using
+    the backwards automatic differentiation.
+
+    :param node: Node for the gradient calculation
+    :param data: data for the computation. NaN values are implicitely marginalized out
+    :param gradients_results: optional for storing the intermediate gradients
+    :param lls_per_node: optional for storing the intermediate results
+    '''
+
+    q = collections.deque()
+    bfs(node, lambda x: q.append(x))
+
+    if not lls_per_node:
+        lls_per_node = np.full((data.shape[0], len(q)), np.nan)
+    lls = log_likelihood(node, data, lls_matrix=lls_per_node)
+
+    gradients = np.exp(gradient_backward(node, lls_per_node))
+
+    node_gradients = []
+
+    for i, spn_node in enumerate(q):
+        if isinstance(spn_node, Leaf):
+            result = node_gradient_functions[type(spn_node)](spn_node, data)
+            node_gradients.append(result * gradients[:, i].reshape(-1,1))
+
+    node_gradients = np.array(node_gradients)
+
+    return np.nansum(node_gradients, axis=0)
+
+
+def conditional_gradient(node, data, evidence_scope, node_gradient_functions=_node_gradient):
+    '''
+    Calculates the conditional gradient with reagrd to the evidence features. This can also be calculated
+    by using a conditioned spn
+    :param node:
+    :param data:
+    :param evidence_scope:
+    :param node_gradient_functions:
+    :return:
+    '''
+
+    marg_evidence = marginalize(node, evidence_scope)
+
+    lls_evidence = np.full((data.shape[0], get_number_of_nodes(marg_evidence)), np.nan)
+
+    gradients_evidence = feature_gradient(marg_evidence, data, node_gradient_functions=node_gradient_functions, lls_per_node=lls_evidence)
+
+    lls_full = np.full((data.shape[0], get_number_of_nodes(node)), np.nan)
+
+    gradients_full = feature_gradient(node, data, node_gradient_functions=node_gradient_functions, lls_per_node=lls_full)
+
+    ll_evidence = lls_evidence[:, 0:1]
+
+    ll_full = lls_full[:, 0:1]
+
+    return (gradients_full * ll_evidence + gradients_evidence * ll_full) / (ll_evidence ** 2)
--- src/spn/tests/test_gradients.py
+++ src/spn/tests/test_gradients.py
@@ -7,10 +7,11 @@ from spn.algorithms.Validity import is_valid
 from spn.algorithms.Inference import likelihood
 from spn.structure.leaves.histogram.Histograms import Histogram
 from spn.structure.leaves.piecewise.PiecewiseLinear import PiecewiseLinear
+from spn.algorithms.Gradient import feature_gradient
 
 
 class TestGradient(unittest.TestCase):
-    def redo_test_piecewise_linear_simple(self):
+    def test_piecewise_linear_simple(self):
         piecewise_spn = 0.5 * PiecewiseLinear([0, 1, 2], [0, 1, 0], [], scope=[0]) + 0.5 * PiecewiseLinear(
             [-2, -1, 0], [0, 1, 0], [], scope=[0]
         )
@@ -18,7 +19,7 @@ class TestGradient(unittest.TestCase):
 
         evidence = np.array([[0.5], [1.5], [-0.5], [-1.5]])
 
-        results = gradient_forward(piecewise_spn, evidence)
+        results = feature_gradient(piecewise_spn, evidence)
         expected_results = np.array([[0.5], [-0.5], [-0.5], [0.5]])
 
         for i, _ in enumerate(evidence):
@@ -27,7 +28,7 @@ class TestGradient(unittest.TestCase):
                 "Expected result was {}, but computed result was {}".format(expected_results[i], results[i]),
             )
 
-    def redo_test_piecewise_linear_combined(self):
+    def test_piecewise_linear_combined(self):
         piecewise_spn = (
             0.5 * PiecewiseLinear([0, 1, 2], [0, 1, 0], [], scope=[0])
             + 0.5 * PiecewiseLinear([-2, -1, 0], [0, 1, 0], [], scope=[0])
@@ -38,16 +39,13 @@ class TestGradient(unittest.TestCase):
 
         self.assertTrue(is_valid(piecewise_spn))
 
-        evidence = np.array([[0.5, 0], [100, 36], [-0.5, -0.5], [-1.5, 0.5]])
-        results = gradient_forward(piecewise_spn, evidence)
-        expected_results = np.array([[0.25, 0.125], [0, 0], [-0.125, 0.125], [0.25, 0]])
+        evidence = np.array([[0.5, 0], [-0.5, -0.5], [-1.5, 0.5]])
+        results = feature_gradient(piecewise_spn, evidence)
+        expected_results = np.array([[0.25, 0.125], [-0.125, 0.125], [0.25, 0]])
 
-        for i, _ in enumerate(evidence):
-            self.assertTrue(
-                np.all(np.equal(results[i], expected_results[i])),
-                "Expected result was {}, but computed result was {}".format(expected_results[i], results[i]),
-            )
-
+        self.assertTrue(
+            np.all(np.isclose(results, expected_results, atol=0.000001)),
+            "Expected result was {}, but computed result was {}".format(expected_results, results))
 
 if __name__ == "__main__":
     unittest.main()
