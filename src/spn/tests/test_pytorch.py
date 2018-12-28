4f02ee7d10461374842ddcb6c25af636fd61932f
pranavsubramani <pranav0212.s@gmail.com>
2018-12-27 21:57:11 -0700

    computation for pytorch tested for binary nodes

---
+++ src/spn/tests/test_pytorch.py
@@ -0,0 +1,46 @@
+'''
+Testcase for PyTorch computation backend
+'''
+
+import numpy as np
+import torch
+from spn.gpu.PyTorch import Param
+from spn.gpu.PyTorch import Network
+from spn.gpu.PyTorch import Nodes
+from torch import optim, cuda
+
+parameters = Param.Param()
+
+
+def create_network1():
+    net = Network.LayerwiseSPN(is_cuda=cuda.is_available())
+
+    leaves = net.add_binary_nodes(2)
+
+    sum1 = net.add_sum_nodes(3)
+
+    weights1 = np.array([[2,8,0,0],
+                        [1,9,0,0],
+                        [0,0,4,6]], dtype='float32').T
+
+    edges1 = net.add_sum_edges(lower_level=leaves, upper_level=sum1, weights=weights1, parameters=parameters)
+
+    prod1 = net.add_product_nodes(2)
+    mask1 = np.array([[1,0],
+                      [0,1],
+                      [1,1]])
+    edges2 = net.add_product_edges(sum1, prod1, connections=mask1)
+
+    sum_final = net.add_sum_nodes(1)
+    weights_final = np.array([[.3, .7]], dtype='float32').T
+    edges_final = net.add_sum_edges(lower_level=prod1, upper_level=sum_final, weights=weights_final,
+                                    parameters=parameters)
+
+    return (leaves, net)
+
+(leaves, net) = create_network1()
+p = net.compute_unnormalized({leaves: np.array([[0,1],[1,0]], dtype='float32')})
+Z = net.compute_unnormalized({leaves: np.ones((2, 2)).astype('float32')})
+
+print('p:', p)
+print('Z:', Z)
