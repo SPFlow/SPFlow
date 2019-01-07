4e2e8c9663f0d85083b2da5549276e371c3946ca
Steven Lang <steven.lang.mz@gmail.com>
2019-01-07 21:19:21 +0100

    Add custom categorical_to_tf_graph
    Fix categorical to tf graph for classification problem.
    
    For a binary class label, there will be two categorical leaf nodes in the SPN.
    One which one-hot encodes the first class as [0, 1] and one that encodes the
    second clas as [1, 0].
    
    Since the tf optimizes the log likelihood, these one-hot represented
    probabilities will be projected into logspace which results in
    log([1,0])=[0, -inf] and therefore NaNs in further computations.
    
    Therefore, this custom method adds a small epsilon, such that the zero
    probability value in the one-hot vector will not degrade to negative infinity.

--- src/spn/algorithms/sklearn.py
+++ src/spn/algorithms/sklearn.py
@@ -11,8 +11,7 @@ from spn.algorithms.LearningWrappers import learn_classifier, learn_parametric
 from spn.algorithms.MPE import mpe
 from spn.gpu.TensorFlow import optimize_tf
 from spn.structure.Base import Context, get_nodes_by_type
-from spn.structure.leaves.parametric.Parametric import (Categorical, Gaussian,
-                                                        Parametric)
+from spn.structure.leaves.parametric.Parametric import Categorical, Gaussian, Parametric
 
 logger = logging.getLogger(__name__)
 
@@ -137,3 +136,39 @@ class SPNClassifier(BaseEstimator, ClassifierMixin):
         for parameter, value in parameters.items():
             setattr(self, parameter, value)
         return self
+
+
+def classification_categorical_to_tf_graph(
+    node, data_placeholder=None, log_space=True, variable_dict=None, dtype=np.float32
+):
+    """
+    Fix categorical to tf graph for classification problem.
+
+    For a binary class label, there will be two categorical leaf nodes in the SPN. One which one-hot encodes the first
+    class as [0, 1] and one that encodes the second clas as [1, 0]. 
+    
+    Since the tf optimizes the log likelihood, these one-hot represented probabilities will be projected into logspace
+    which results in log([1,0])=[0, -inf] and therefore NaNs in further computations.
+
+    Therefore, this custom method adds a small epsilon, such that the zero probability value in the one-hot vector will
+    not degrade to negative infinity.
+    """
+    with tf.variable_scope("%s_%s" % (node.__class__.__name__, node.id)):
+        p = np.array(node.p, dtype=dtype)
+
+        # Epsilon to make sure there are no zero values
+        eps = 1e-20
+        p += eps
+
+        # Renormalize such that the sum over all probabilities is one
+        p /= np.sum(p)
+
+        assert np.all(p > 0), "Probabilities in the class leaf nodes have to be greater than zero but were %s" % p
+
+        softmaxInverse = np.log(p / np.max(p)).astype(dtype)
+        probs = tf.nn.softmax(tf.constant(softmaxInverse))
+        variable_dict[node] = probs
+        if log_space:
+            return tf.distributions.Categorical(probs=probs).log_prob(data_placeholder[:, node.scope[0]])
+
+        return tf.distributions.Categorical(probs=probs).prob(data_placeholder[:, node.scope[0]])
