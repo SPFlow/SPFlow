cfc64e70ebe6ef123041a7e16ef013f2851ad0c2
Steven Lang <steven.lang.mz@gmail.com>
2019-01-08 14:09:52 +0100

    Add optional log_bottom_up_lambda for mpe

--- src/spn/algorithms/MPE.py
+++ src/spn/algorithms/MPE.py
@@ -62,10 +62,15 @@ def log_node_bottom_up_mpe(node, *args, **kwargs):
         return np.log(probs)
 
 
-def add_node_mpe(node_type, bottom_up_lambda, top_down_lambda):
+def add_node_mpe(node_type, bottom_up_lambda, top_down_lambda, log_bottom_up_lambda=None):
     _node_top_down_mpe[node_type] = top_down_lambda
     _node_bottom_up_mpe[node_type] = bottom_up_lambda
-    _node_bottom_up_mpe_log[node_type] = log_node_bottom_up_mpe
+
+    # If no log_bottom_up_lambda method is given, use the generic log_node_buttom_up_mpe method
+    if log_bottom_up_lambda is None:
+        _node_bottom_up_mpe_log[node_type] = log_node_bottom_up_mpe
+    else:
+        _node_bottom_up_mpe_log[node_type] = log_bottom_up_lambda
 
 
 def mpe(
