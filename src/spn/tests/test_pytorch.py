4d18c5242c7b5e51776f12c0ebf5f8441c9357e0
Pranav Subramani <pranav0212.s@gmail.com>
2019-01-10 22:35:55 -0500

    Adding the testcase for the file and removing unnecessary variables
    Former-commit-id: e1d0abcc7798b4367a8eb6abe25979ddc1a7928a

--- src/spn/tests/test_pytorch.py
+++ src/spn/tests/test_pytorch.py
@@ -3,12 +3,9 @@ Testcase for PyTorch computation backend
 '''
 
 import numpy as np
-import torch
+from torch import cuda
 from spn.gpu.PyTorch import Param
 from spn.gpu.PyTorch import Network
-from spn.gpu.PyTorch import Nodes
-from torch import optim, cuda
-
 parameters = Param.Param()
 
 
@@ -19,27 +16,29 @@ def create_network1():
 
     sum1 = net.add_sum_nodes(3)
 
-    weights1 = np.array([[2,8,0,0],
-                        [1,9,0,0],
-                        [0,0,4,6]], dtype='float32').T
+    weights1 = np.array([[2, 8, 0, 0],
+                         [1, 9, 0, 0],
+                         [0, 0, 4, 6]], dtype='float32').T
 
-    edges1 = net.add_sum_edges(lower_level=leaves, upper_level=sum1, weights=weights1, parameters=parameters)
+    net.add_sum_edges(lower_level=leaves, upper_level=sum1,
+                      weights=weights1, parameters=parameters)
 
     prod1 = net.add_product_nodes(2)
-    mask1 = np.array([[1,0],
-                      [0,1],
-                      [1,1]])
-    edges2 = net.add_product_edges(sum1, prod1, connections=mask1)
+    mask1 = np.array([[1, 0],
+                      [0, 1],
+                      [1, 1]])
+    net.add_product_edges(sum1, prod1, connections=mask1)
 
     sum_final = net.add_sum_nodes(1)
     weights_final = np.array([[.3, .7]], dtype='float32').T
-    edges_final = net.add_sum_edges(lower_level=prod1, upper_level=sum_final, weights=weights_final,
-                                    parameters=parameters)
+    net.add_sum_edges(lower_level=prod1, upper_level=sum_final, weights=weights_final,
+                      parameters=parameters)
 
     return (leaves, net)
 
+
 (leaves, net) = create_network1()
-p = net.compute_unnormalized({leaves: np.array([[0,1],[1,0]], dtype='float32')})
+p = net.compute_unnormalized({leaves: np.array([[0, 1], [1, 0]], dtype='float32')})
 Z = net.compute_unnormalized({leaves: np.ones((2, 2)).astype('float32')})
 
 print('p:', p)
