fix max log likelihood calculation for missing dicision branches

src/spn/algorithms/Inference.py
@@ -50,14 +50,20 @@ def max_log_likelihood(node, children, data=None, dtype=np.float64):
     assert data is not None, "data must be passed through to max nodes for proper evaluation."
     decision_value_given = data[:, node.dec_idx]
     max_value = np.argmax(llchildren, axis=1)
+    d_given = np.full(decision_value_given.shape[0], np.nan)
+    mapd = {node.dec_values[i]:i for i in range(len(node.dec_values))}
+    for k, v in mapd.items(): d_given[decision_value_given==k] = v
     # if data contains a decision value use that otherwise use max
-    child_idx = np.select([np.isnan(decision_value_given), True],
-                          [max_value, decision_value_given]).astype(int)
-    # print(child_idx)
-    # print(llchildren)
-    mll = llchildren[np.arange(llchildren.shape[0]), child_idx].reshape(-1, 1)
-    return mll
+    child_idx = np.select([np.isnan(d_given), True],
+                          [max_value, d_given]).astype(int)
 
+    mll = llchildren[np.arange(llchildren.shape[0]), child_idx].reshape(-1, 1)
+
+    # if decision value given is not in children, assign 0 probability
+    missing_dec_branch = np.logical_and(np.logical_not(np.isnan(decision_value_given)),np.isnan(d_given))
+    mll[missing_dec_branch] = np.finfo(mll.dtype).min
+
+    return mll
 
 def max_likelihood(node, children, data=None, dtype=np.float64):
     llchildren = np.concatenate(children, axis=1)