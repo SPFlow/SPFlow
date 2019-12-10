9179ba24d7420facccf2cc452f1c1024283b6bb1
minimum-LaytonC <laytonc.hayes@yahoo.com>
2019-12-10 17:16:12 -0500

    adds function to get best decision based on meu given input data

--- src/spn/algorithms/MEU.py
+++ src/spn/algorithms/MEU.py
@@ -33,11 +33,11 @@ def meu_max(node, meu_per_node, data=None, lls_per_node=None, rand_gen=None):
     decision_value_given = data[:, node.dec_idx]
     max_value = np.argmax(meu_children, axis=1)
     # if data contains a decision value use that otherwise use max
-    child_idx = np.select([np.isnan(decision_value_given), True],
+    dec_value = np.select([np.isnan(decision_value_given), True],
                           [max_value, decision_value_given]).astype(int)
-    child_idx_to_id = lambda idx: node.children[idx].id
-    child_idx_to_id = np.vectorize(child_idx_to_id)
-    child_id = child_idx_to_id(child_idx)
+    dec_value_to_child_id = lambda idx: node.children[node.dec_values.index(idx)].id
+    dec_value_to_child_id = np.vectorize(dec_value_to_child_id)
+    child_id = dec_value_to_child_id(dec_value)
     meu_per_node[:,node.id] = meu_per_node[np.arange(meu_per_node.shape[0]),child_id]
 
 def meu_util(node, meu_per_node, data=None, lls_per_node=None, rand_gen=None):
@@ -60,7 +60,7 @@ def meu(node, input_data,
     if in_place:
         data = input_data
     else:
-        data = np.array(input_data)
+        data = np.copy(input_data)
     # assumes utility is only one and is at the last
     # print("input data:", input_data[:, -1])
     assert np.isnan(data[:, -1]), "Please specify utility variable as NaN"
@@ -167,3 +167,37 @@ def eval_spmn_top_down_meu(root, eval_functions,
             delattr(node_type, "_eval_func")
 
     return all_results[root], all_decisions, all_max_nodes
+
+def best_next_decision(root, input_data, in_place=False):
+    if in_place:
+        data = input_data
+    else:
+        data = np.copy(input_data)
+    nodes = get_nodes_by_type(root)
+    dec_dict = {}
+    # find all possible decision values
+    for node in nodes:
+        if type(node) == Max:
+            if node.dec_idx in dec_dict:
+                dec_dict[node.dec_idx].union(set(node.dec_values))
+            else:
+                dec_dict[node.dec_idx] = set(node.dec_values)
+    next_dec_idx = None
+    # find next undefined decision
+    for idx in dec_dict.keys():
+        if np.all(np.isnan(data[:,idx])):
+            next_dec_idx = idx
+            break
+    assert next_dec_idx != None, "please assign all values of next decision to np.nan"
+    # determine best decisions based on meu
+    dec_vals = list(dec_dict[next_dec_idx])
+    best_decisions = np.full((1,data.shape[0]),dec_vals[0])
+    data[:,next_dec_idx] = best_decisions
+    meu_best = meu(root, data)
+    for i in range(1, len(dec_vals)):
+        decisions_i = np.full((1,data.shape[0]), dec_vals[i])
+        data[:,next_dec_idx] = decisions_i
+        meu_i = meu(root, data)
+        best_decisions = np.select([np.greater(meu_i, meu_best),True],[decisions_i, best_decisions])
+        meu_best = np.maximum(meu_i,meu_best)
+    return best_decisions
