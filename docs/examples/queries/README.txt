dbe98699d39cad59aeae966cba23e398a22a86ce
Alexander L. Hayes <alexander@batflyer.net>
2020-06-04 10:54:49 -0400

    📝 Querying Probabilistic Models Examples
    Add `examples/queries/README.txt`
    Add `plot_marginalize.py` showing marginal SPN example
    Add `plot_tractable_inference.py` showing marginal queries
    Add reference line to `plot_spn_object_hierarchy`
    
    These examples also pull from the project README, but are grouped
    so documentation about querying probabilistic models can be
    put in a common place and reasoned about individually.
    
    
    Former-commit-id: 936b1475a61fb8e728bbcdce23d9a81752b8abe8

---
+++ docs/examples/queries/README.txt
@@ -0,0 +1,7 @@
+.. _probabilistic_queries:
+
+Querying Probabilistic Models
+-----------------------------
+
+One of the most interesting aspect of SPNs is the tractable inference.
+These examples demonstrate some of the ways these models may be queried.
--- docs/examples/queries/plot_marginalize.py
+++ docs/examples/queries/plot_marginalize.py
@@ -1,13 +1,14 @@
 """
-.. _composing_spn_object_hierarchy:
+.. _marginalizing_an_spn:
 
-=====================================
-Composing SPNs from Sums and Products
-=====================================
+====================
+Marginalizing an SPN
+====================
 
-The alternative to the DSL is to construct SPNs directly from ``Sum`` and
-``Product`` objects, demonstrated here:
+"Marginalizing an SPN" means summing out all other non-relevant variables.
 """
+
+# sphinx_gallery_thumbnail_number = 2
 
 from spn.structure.leaves.parametric.Parametric import Categorical
 from spn.structure.Base import Sum, Product
@@ -16,6 +17,9 @@ from spn.structure.Base import assign_ids, rebuild_scopes_bottom_up
 from spn.io.Graphics import draw_spn
 import matplotlib.pyplot as plt
 
+# %%
+# We will start with the Sum-Product Network structure from the
+# :ref:`composing_spn_object_hierarchy` example.
 
 p0 = Product(children=[Categorical(p=[0.3, 0.7], scope=1), Categorical(p=[0.4, 0.6], scope=2)])
 p1 = Product(children=[Categorical(p=[0.5, 0.5], scope=1), Categorical(p=[0.6, 0.4], scope=2)])
@@ -29,3 +33,18 @@ assign_ids(spn)
 rebuild_scopes_bottom_up(spn)
 
 ax = draw_spn(spn)
+
+# %%
+# If we want to marginalize this SPN by summing out all other variables
+# to leave variables 1 and 2, we can do this as follows:
+
+from spn.algorithms.Marginalization import marginalize
+
+spn_marg = marginalize(spn, [1, 2])
+
+# %%
+# This marginalizes all the variables *not* in :math:`[1, 2]`, and create a
+# *new* structure that knows nothing about the previous one nor about the
+# variable 0.
+
+draw_spn(spn_marg)
