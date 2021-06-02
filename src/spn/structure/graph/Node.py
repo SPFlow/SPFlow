dcec00bda6c67a9174c9f90da03300f98426361d
Philipp Deibert <pdeibert@pm.me>
2021-06-02 15:55:03 +0200

    Equal operator for Nodes

--- src/spn/structure/graph/Node.py
+++ src/spn/structure/graph/Node.py
@@ -6,7 +6,7 @@ Created on May 05, 2021
 This file provides the basic components to build abstract probabilistic circuits, like SumNode, ProductNode, and LeafNode.
 """
 from typing import List, Optional
-
+import math
 
 class Node:
     """Base class for all types of nodes
@@ -27,19 +27,24 @@ class Node:
         '''
         Ad-hoc method to print structure of node and children (for debugging purposes)
         '''
-        #strings = [self.name]
         strings = [self.__class__.__name__]
         strings += [str(child).replace("\n", "\n    ") for child in self.children]
 
         return "\n    ".join(strings)
 
+    def __eq__(self, other) -> bool:
+        '''
+        Checks whether two objects are identical by comparing their class, scope and children (recursively). 
+        '''
+        return (type(self) is type(other)
+            and self.scope == other.scope
+            and self.children == other.children)
 
 class ProductNode(Node):
     """A ProductNode provides a factorization of its children, i.e. product nodes in SPNs have children with distinct scopes"""
 
     def __init__(self, children: List[Node], scope: List[int]) -> None:
         super().__init__(children=children, scope=scope)
-
 
 class SumNode(Node):
     """A SumNode provides a weighted mixture of its children, i.e. sum nodes in SPNs have children with identical scopes
@@ -54,6 +59,16 @@ class SumNode(Node):
     ) -> None:
         super().__init__(children=children, scope=scope)
         self.weights = weights
+    
+    def __eq__(self, other) -> bool:
+        '''
+        Checks whether two objects are identical by comparing their class, scope, children (recursively) and weights.
+        Note that weight comparison is done approximately due to numerical issues when conversion between graph representations.
+        '''
+        return (super().__eq__(other)
+            and all(map(lambda x,y: math.isclose(x,y,rel_tol=1.e-5), self.weights, other.weights))
+            and len(self.weights) == len(other.weights))
+
 
 class LeafNode(Node):
     """A LeafNode provides a probability distribution over some input variable(s)"""
