be1989b937f16e5723a2a94b0f78d5fbdfb73f0a
Huy-DC <khuynguyen96@gmail.com>
2021-07-18 16:24:25 +0200

    removes positional arguments, and kwargs, args for dipatched methods

--- src/spn/backend/pytorch/rat/inference.py
+++ src/spn/backend/pytorch/rat/inference.py
@@ -2,11 +2,13 @@ from .rat_spn import TorchRatSpn
 from multipledispatch import dispatch
 import torch
 
+
 @dispatch(TorchRatSpn, torch.Tensor)
 def log_likelihood(rat_spn: TorchRatSpn, data: torch.Tensor) -> torch.Tensor:
     # make sure data has double precision
     data = data.double()
     return rat_spn.forward(data)
+
 
 @dispatch(TorchRatSpn, torch.Tensor)
 def likelihood(rat_spn: TorchRatSpn, data: torch.Tensor) -> torch.Tensor:
