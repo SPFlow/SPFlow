41ee9e75c2ad16fab48b0fa9639eb9ba5a2bbc9e
Philipp Deibert <pdeibert@pm.me>
2021-07-20 10:35:09 +0200

    Revised weighted LogSumExp and torch tensor dtypes

--- src/spn/backend/pytorch/rat/inference.py
+++ src/spn/backend/pytorch/rat/inference.py
@@ -1,12 +1,12 @@
 from .rat_spn import TorchRatSpn
-from multipledispatch import dispatch
+from multipledispatch import dispatch  # type: ignore
 import torch
+
 
 @dispatch(TorchRatSpn, torch.Tensor)
 def log_likelihood(rat_spn: TorchRatSpn, data: torch.Tensor) -> torch.Tensor:
-    # make sure data has double precision
-    data = data.double()
     return rat_spn.forward(data)
+
 
 @dispatch(TorchRatSpn, torch.Tensor)
 def likelihood(rat_spn: TorchRatSpn, data: torch.Tensor) -> torch.Tensor:
