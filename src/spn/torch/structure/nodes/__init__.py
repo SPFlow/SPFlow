4b6e41f5dc24f303326250679d4e7716057b1b7f
Philipp Deibert <pdeibert@pm.me>
2021-08-17 13:38:06 +0200

    Black formatting

--- src/spn/torch/structure/nodes/__init__.py
+++ src/spn/torch/structure/nodes/__init__.py
@@ -1,2 +1,18 @@
 from .node import TorchNode, TorchSumNode, TorchProductNode, TorchLeafNode, toTorch, toNodes
-from .leaves.parametric import TorchGaussian, TorchLogNormal, TorchMultivariateGaussian, TorchUniform, TorchGeometric, TorchHypergeometric, TorchGamma, TorchBernoulli, TorchBinomial, TorchNegativeBinomial, TorchExponential, TorchPoisson, TorchParametricLeaf, toNodes, toTorch
+from .leaves.parametric import (
+    TorchGaussian,
+    TorchLogNormal,
+    TorchMultivariateGaussian,
+    TorchUniform,
+    TorchGeometric,
+    TorchHypergeometric,
+    TorchGamma,
+    TorchBernoulli,
+    TorchBinomial,
+    TorchNegativeBinomial,
+    TorchExponential,
+    TorchPoisson,
+    TorchParametricLeaf,
+    toNodes,
+    toTorch,
+)
