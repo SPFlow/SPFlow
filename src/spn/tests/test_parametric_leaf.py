e80d3571cc4f7a957512526f3fc6f1512da0e910
Philipp Deibert <pdeibert@pm.me>
2021-09-13 09:02:57 +0200

    Added Multivariate Gaussian tests

--- src/spn/tests/test_parametric_leaf.py
+++ src/spn/tests/test_parametric_leaf.py
@@ -124,7 +124,57 @@ class TestParametricLeaf(unittest.TestCase):
         self.assertTrue(np.allclose(probs, targets))
 
     def test_multivariate_gaussian(self):
-        pass
+
+        # ----- configuration 1 -----
+        mean_vector = np.zeros(2)
+        covariance_matrix = np.eye(2)
+
+        multivariate_gaussian = MultivariateGaussian(
+            [0], mean_vector.tolist(), covariance_matrix.tolist()
+        )
+
+        # create test inputs/outputs
+        data = np.stack([np.zeros(2), np.ones(2)], axis=0)
+
+        targets = np.array([[0.1591549], [0.0585498]])
+
+        probs = likelihood(multivariate_gaussian, data)
+        log_probs = log_likelihood(multivariate_gaussian, data)
+
+        self.assertTrue(np.allclose(probs, np.exp(log_probs)))
+        self.assertTrue(np.allclose(probs, targets))
+
+        # ----- configuration 2 -----
+        mean_vector = np.arange(3)
+        covariance_matrix = np.array(
+            [
+                [2, 2, 1],
+                [2, 3, 2],
+                [1, 2, 3],
+            ]
+        )
+
+        multivariate_gaussian = MultivariateGaussian(
+            [0, 1, 2], mean_vector.tolist(), covariance_matrix.tolist()
+        )
+
+        # create test inputs/outputs
+        data = np.stack(
+            [
+                mean_vector,
+                np.ones(3),
+                -np.ones(3),
+            ],
+            axis=0,
+        )
+
+        targets = np.array([[0.0366580], [0.0159315], [0.0081795]])
+
+        probs = likelihood(multivariate_gaussian, data)
+        log_probs = log_likelihood(multivariate_gaussian, data)
+
+        self.assertTrue(np.allclose(probs, np.exp(log_probs)))
+        self.assertTrue(np.allclose(probs, targets))
 
     def test_uniform(self):
 
