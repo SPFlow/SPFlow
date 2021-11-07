Fix log computation form torch sum and product nodes

src/spflow/torch/structure/nodes/node.py
@@ -16,12 +16,15 @@ import torch.nn as nn
 from spflow.torch.structure.module import TorchModule
 from spflow.base.structure.nodes.node import Node, ISumNode, IProductNode, ILeafNode
 
+
 def proj_convex_to_real(x: torch.Tensor) -> torch.Tensor:
     # convex coefficients are already normalized, so taking the log is sufficient
     return torch.log(x)
 
+
 def proj_real_to_convex(x: torch.Tensor) -> torch.Tensor:
     return torch.nn.functional.softmax(x, dim=-1)
+
 
 class TorchNode(TorchModule):
     """PyTorch version of an abstract node. See Node.
@@ -70,16 +73,16 @@ class TorchSumNode(TorchNode):
         self,
         children: List[TorchModule],
         scope: List[int],
-        weights: Union[np.ndarray, torch.Tensor] = np.empty(0),
+        weights: Union[np.ndarray, torch.Tensor, List[float]] = np.empty(0),
         normalize: bool = True,
     ) -> None:
 
         if not children:
             raise ValueError("Sum node must have at least one child.")
-        
+
         if weights is None:
             weights = torch.rand(sum(len(child) for child in children))
-        elif(isinstance(weights, np.ndarray)):
+        elif isinstance(weights, np.ndarray) or isinstance(weights, list):
             weights = torch.tensor(weights)
 
         if not torch.all(weights >= 0):
@@ -96,25 +99,24 @@ class TorchSumNode(TorchNode):
 
         # store auxiliary weights as torch parameters
         self.weights_aux = nn.Parameter(weights.log())
-    
-    def __getattr__(self, attr: str) -> Any:
-        if(attr == "weights"):
-            # project auxiliary weights onto weights that sum up to one
-            return proj_real_to_convex(self.weights_aux)
-        else:
-            return nn.Module.__getattr__(self, attr)
-    
-    def __setattr__(self, attr: str, value: Any) -> None:
-        if(attr == "weights"):
-            # TODO: make sure that weights (approximately) sum up to one and have correct length
-            self.weights_aux.data = proj_convex_to_real(value)
-        else:
-            nn.Module.__setattr__(self, attr, value)
 
-    def forward(self, x):
-        # TODO: broadcast across batches
-        # return weighted sum
-        return (x * self.weights).sum()
+    @property
+    def weights(self) -> torch.Tensor:
+        # project auxiliary weights onto weights that sum up to one
+        return proj_real_to_convex(self.weights_aux)
+
+    @weights.setter
+    def weights(self, value: Union[np.ndarray, torch.Tensor, List[float]]) -> None:
+        # TODO: possible checks
+        self.weights_aux.data = proj_convex_to_real(value)  # type: ignore
+
+    def forward(self, data: torch.Tensor) -> torch.Tensor:
+        # get inputs recursively
+        inputs = torch.hstack([child(data) for child in self.children()])  # type: ignore
+        # weight inputs in log-space
+        weighted_inputs = inputs + self.weights.log()  # type: ignore
+
+        return torch.logsumexp(weighted_inputs, dim=-1)
 
 
 @dispatch(ISumNode)  # type: ignore[no-redef]
@@ -148,8 +150,8 @@ class TorchProductNode(TorchNode):
 
     def forward(self, x):
         # TODO: broadcast across batches
-        # return weighted product
-        return x.prod()
+        # return product (sum in log space)
+        return x.sum()
 
 
 @dispatch(IProductNode)  # type: ignore[no-redef]