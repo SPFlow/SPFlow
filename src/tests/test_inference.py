6f8d9951b1f56dfa2c8a91a993fcc1bc5a9cd337
Huy-DC <khuynguyen96@gmail.com>
2021-11-21 19:15:41 +0100

    Adds tests for mixed modules and some smaller tests

--- src/tests/test_inference.py
+++ src/tests/test_inference.py
@@ -1,8 +1,9 @@
 import unittest
 from spflow.base.inference.nodes import likelihood, log_likelihood
 from spflow.base.structure.nodes.leaves.parametric import Gaussian
+from spflow.base.inference.nodes.leaves.parametric.parametric import node_likelihood, node_log_likelihood
 import numpy as np
-from spflow.base.structure.nodes import ISumNode, IProductNode
+from spflow.base.structure.nodes import ISumNode, IProductNode, INode
 from spflow.base.structure.network_type import SPN
 
 
@@ -206,6 +207,22 @@ class TestInference(unittest.TestCase):
 
         result = log_likelihood(SPN(), spn, np.array([np.nan, 0.0, 1.0]).reshape(-1, 3))
         self.assertAlmostEqual(result[0][0], -2.33787707)
+
+    def test_not_implemented(self):
+        class ITestNode(INode):
+            """A Internal ProductNode provides a factorization of its children,
+            i.e. IProductNodes in SPNs have children with distinct scopes"""
+
+            def __init__(self, children, scope) -> None:
+                super().__init__(children=children, scope=scope)
+
+        with self.assertRaises(NotImplementedError):
+            spn = ITestNode([], [0])
+            probs = node_likelihood(spn, data=np.array([1.0]).reshape(-1, 1))
+
+        with self.assertRaises(NotImplementedError):
+            spn = ITestNode([], [0])
+            probs = node_log_likelihood(spn, data=np.array([1.0]).reshape(-1, 1))
 
 
 if __name__ == "__main__":
