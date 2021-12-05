e35612277fbefc48047b381e210a5654c7bc951d
Philipp Deibert <pdeibert@pm.me>
2021-12-05 16:11:50 +0100

    Reorganize parametric leaf tests

---
+++ src/tests/parametric_leaf/test_log_normal.py
@@ -0,0 +1,100 @@
+from spflow.base.structure.nodes.leaves.parametric import LogNormal
+from spflow.base.inference.nodes.node import likelihood, log_likelihood
+from spflow.base.structure.network_type import SPN
+import numpy as np
+
+import unittest
+import random
+
+
+class TestLogNormal(unittest.TestCase):
+    def test_log_normal(self):
+
+        # ----- configuration 1 -----
+        mean = 0.0
+        stdev = 0.25
+
+        log_normal = LogNormal([0], mean, stdev)
+
+        # create test inputs/outputs
+        data = np.array([[0.5], [1.0], [1.5]])
+        targets = np.array([[0.0683495], [1.59577], [0.285554]])
+
+        probs = likelihood(log_normal, data, SPN())
+        log_probs = log_likelihood(log_normal, data, SPN())
+
+        self.assertTrue(np.allclose(probs, np.exp(log_probs)))
+        self.assertTrue(np.allclose(probs, targets))
+
+        # ----- configuration 2 -----
+        mean = 0.0
+        stdev = 0.5
+
+        log_normal = LogNormal([0], mean, stdev)
+
+        # create test inputs/outputs
+        data = np.array([[0.5], [1.0], [1.5]])
+        targets = np.array([[0.610455], [0.797885], [0.38287]])
+
+        probs = likelihood(log_normal, data, SPN())
+        log_probs = log_likelihood(log_normal, data, SPN())
+
+        self.assertTrue(np.allclose(probs, np.exp(log_probs)))
+        self.assertTrue(np.allclose(probs, targets))
+
+        # ----- configuration 3 -----
+        mean = 0.0
+        stdev = 1.0
+
+        log_normal = LogNormal([0], mean, stdev)
+
+        # create test inputs/outputs
+        data = np.array([[0.5], [1.0], [1.5]])
+        targets = np.array([[0.627496], [0.398942], [0.244974]])
+
+        probs = likelihood(log_normal, data, SPN())
+        log_probs = log_likelihood(log_normal, data, SPN())
+
+        self.assertTrue(np.allclose(probs, np.exp(log_probs)))
+        self.assertTrue(np.allclose(probs, targets))
+
+        # ----- support -----
+        mean = 0.0
+        stdev = 1.0
+
+        log_normal = LogNormal([0], mean, stdev)
+
+        # create test inputs testing around the boundaries
+        data = np.array(
+            [[-1.0], [0.0], [np.inf], [np.nextafter(0.0, 1.0)], [np.finfo(np.float64).max / 3.0]]
+        )
+
+        probs = likelihood(log_normal, data, SPN())
+        log_probs = log_likelihood(log_normal, data, SPN())
+
+        self.assertTrue(np.allclose(probs, np.exp(log_probs)))
+        self.assertTrue(all(np.isinf(log_probs[:3])))
+        self.assertTrue(all(~np.isinf(log_probs[3:])))
+
+        # ----- invalid parameters -----
+        mean = random.random()
+
+        self.assertRaises(Exception, LogNormal, [0], mean, 0.0)
+        self.assertRaises(Exception, LogNormal, [0], mean, np.nextafter(0.0, -1.0))
+        self.assertRaises(Exception, LogNormal, [0], np.inf, 1.0)
+        self.assertRaises(Exception, LogNormal, [0], np.nan, 1.0)
+        self.assertRaises(Exception, LogNormal, [0], mean, np.inf)
+        self.assertRaises(Exception, LogNormal, [0], mean, np.nan)
+
+        # set parameters to None manually
+        log_normal.stdev = None
+        self.assertRaises(Exception, likelihood, SPN(), log_normal, data)
+        log_normal.mean = None
+        self.assertRaises(Exception, likelihood, SPN(), log_normal, data)
+
+        # invalid scope length
+        self.assertRaises(Exception, LogNormal, [], 0.0, 1.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
