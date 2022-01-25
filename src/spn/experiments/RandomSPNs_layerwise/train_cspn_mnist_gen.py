52377ee2445f81ca4860e862f883fe7912cd71f5
monoclecat <git@andrewdelay.com>
2022-01-25 09:39:22 +0100

    Add option to use RATSPN in mnist gen experiment for comparison.

--- src/spn/experiments/RandomSPNs_layerwise/train_cspn_mnist_gen.py
+++ src/spn/experiments/RandomSPNs_layerwise/train_cspn_mnist_gen.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 
 from spn.experiments.RandomSPNs_layerwise.distributions import RatNormal
 from spn.experiments.RandomSPNs_layerwise.cspn import CSPN, CspnConfig
+from spn.experiments.RandomSPNs_layerwise.rat_spn import RatSpn, RatSpnConfig
 
 from train_mnist import count_params, ensure_dir, set_seed
 
@@ -92,14 +93,21 @@ def evaluate_model(model, save_dir, device, loader, tag):
     model.eval()
     log_like = []
     label = torch.as_tensor(np.arange(10)).repeat_interleave(10)
-    label = F.one_hot(label, 10).float().to(device)
-    samples = model.sample(condition=label).view(-1, *img_size[1:])
+    if args.ratspn:
+        samples = model.sample(class_index=label.tolist())
+    else:
+        label = F.one_hot(label, 10).float().to(device)
+        samples = model.sample(condition=label)
+    samples = samples.view(-1, *img_size[1:])
     plot_samples(samples, save_dir)
     with torch.no_grad():
         for image, label in loader:
-            label = F.one_hot(label, 10).float().to(device)
             image = image.flatten(start_dim=1).to(device)
-            log_like.append(model(x=image, condition=label).mean().tolist())
+            if args.ratspn:
+                log_like.append(model(x=image).mean().tolist())
+            else:
+                label = F.one_hot(label, 10).float().to(device)
+                log_like.append(model(x=image, condition=label).mean().tolist())
     print("{} set: Average log-likelihood: {:.4f}".format(tag, np.mean(log_like)))
 
 
@@ -151,6 +159,7 @@ if __name__ == "__main__":
     parser.add_argument('--verbose', '-V', action='store_true', help='Output more debugging information when running.')
     parser.add_argument('--inspect', action='store_true', help='Enter inspection mode')
     parser.add_argument('--sumfirst', action='store_true', help='Make first layer after dists a sum layer.')
+    parser.add_argument('--ratspn', action='store_true', help='Use a RATSPN and not a CSPN')
     args = parser.parse_args()
 
     results_dir = os.path.join(args.results_dir, f"results_{args.exp_name}")
@@ -180,126 +189,144 @@ if __name__ == "__main__":
 
     inspect = args.inspect
     if inspect:
-        def plot_img(image: torch.Tensor, title: str = None, path=None):
-            # Tensor shape N x channels x rows x cols
-            tensors = torchvision.utils.make_grid(image, nrow=image.shape[0], padding=1)
-            arr = tensors.permute(1, 2, 0).cpu().numpy()
-            arr = skimage.img_as_ubyte(arr)
-            imageio.imwrite(path, arr)
-            return
-            plt.imshow(arr)
-            plt.title(title, fontdict={'fontsize': 10})
-            plt.show()
+        i = 10
+        base_path = os.path.join('mnist_gen_exp', f'results_mnistgen{i}')
+        path = os.path.join(base_path, 'models', 'epoch-099.pt')
+        model = torch.load(path, map_location=torch.device('cpu'))
 
-        top_5_ll = torch.ones(5) * -10e6
-        top_5_ll_img = torch.zeros(5, *img_size)
-        low_5_ll = torch.ones(5) * 10e6
-        low_5_ll_img = torch.zeros(5, *img_size)
+        exp = 1
+        if exp == 1:
+            results_dir = os.path.join(base_path, 'override_root_sum_sample_choices')
+            if not os.path.exists(results_dir):
+                os.makedirs(results_dir)
+            for d in range(10):
+                if not os.path.exists(os.path.join(results_dir, f'cond_{d}')):
+                    os.makedirs(os.path.join(results_dir, f'cond_{d}'))
+            for d in range(10):
+                cond = torch.ones(model.config.R * model.config.S**2).long() * d
+                cond = F.one_hot(cond, cond_size).float()
+                sample = model.sample(condition=cond)
+                sample[sample < 0.0] = 0.0
+                sample[sample > 1.0] = 1.0
+                # sample_ll = model(x=sample, condition=None).flatten()
+                sample = sample.view(-1, *img_size)
 
-        # path = 'results_stl_1/models/epoch-079.pt'
-        # path = 'results_stl_2/models/epoch-029.pt'
-        # path = 'results_stl_3/models/epoch-049.pt'
-        # path = 'results_stl7/models/epoch-099.pt'
-        path = 'results_stl10/models/epoch-099.pt'
-        # path = [f"results_stl6/models/epoch-099-chan{ch}.pt" for ch in range(3)]
-        results_dir = os.path.join(args.results_dir, f"results_stl10")
-
-        models = []
-        model = None
-        if isinstance(path, list):
-            models = [torch.load(p).cpu() for p in path]
-            spn_per_channel = True
+                for i in range(model.config.R):
+                    b = sample[(i * model.config.S**2):((i + 1) * model.config.S**2), :, :, :]
+                    tensors = torchvision.utils.make_grid(b, nrow=10, padding=1)
+                    arr = tensors.permute(1, 2, 0).cpu().numpy()
+                    arr = skimage.img_as_ubyte(arr)
+                    imageio.imwrite(os.path.join(results_dir, f"cond_{d}", f'rep{i}.png'), arr)
         else:
-            model = torch.load(path).cpu()
-            spn_per_channel = False
+            results_dir = base_path
+            def plot_img(image: torch.Tensor, title: str = None, path=None):
+                # Tensor shape N x channels x rows x cols
+                tensors = torchvision.utils.make_grid(image, nrow=5, padding=1)
+                arr = tensors.permute(1, 2, 0).cpu().numpy()
+                arr = skimage.img_as_ubyte(arr)
+                # imageio.imwrite(path, arr)
+                # return
+                plt.imshow(arr)
+                plt.title(title, fontdict={'fontsize': 10})
+                plt.show()
 
-        show_all = True
-        find_top_low_LL = False
-        if show_all:
-            batch_size = 5
-        else:
-            batch_size = 256
+            top_5_ll = torch.ones(5) * -10e6
+            top_5_ll_img = torch.zeros(5, *img_size)
+            low_5_ll = torch.ones(5) * 10e6
+            low_5_ll_img = torch.zeros(5, *img_size)
 
-        train_loader, test_loader = get_mnist_loaders(args.dataset_dir, args.grayscale,
-                                                      batch_size=batch_size, device=device)
-        for i, (image, cond) in enumerate(train_loader):
-            cond = F.one_hot(cond, cond_size).float()
-            data_ll = model(x=image.flatten(start_dim=1), condition=cond).flatten()
-            sample = model.sample(condition=None)
-            sample_ll = model(x=sample, condition=None).flatten()
 
-            sample[sample < 0.0] = 0.0
-            sample[sample > 1.0] = 1.0
+            show_all = True
+            find_top_low_LL = False
+            if show_all:
+                batch_size = 5
+            else:
+                batch_size = 256
 
-            if find_top_low_LL:
-                top_5_ll, indices = torch.cat((top_5_ll, sample_ll), dim=0).sort(descending=True)
-                top_5_ll = top_5_ll[:5]
-                indices = indices[:5]
-                imgs = []
-                for ind in indices:
-                    img = top_5_ll_img[ind] if ind < 5 else cond[ind-5]
-                    imgs.append(img.unsqueeze(0))
-                top_5_ll_img = torch.cat(imgs, dim=0)
+            # train_loader, test_loader = get_mnist_loaders(args.dataset_dir, args.grayscale, batch_size=batch_size, device=device)
+            for cond in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+                cond = torch.ones(1000).long() * cond
+                cond = F.one_hot(cond, cond_size).float()
+                sample = model.sample(condition=cond)
+                sample[sample < 0.0] = 0.0
+                sample[sample > 1.0] = 1.0
+                sample_ll = model(x=sample, condition=None).flatten()
+                sample = sample.view(-1, *img_size)
 
-                low_5_ll, indices = torch.cat((low_5_ll, sample_ll), dim=0).sort(descending=False)
-                low_5_ll = low_5_ll[:5]
-                indices = indices[:5]
-                imgs = []
-                for ind in indices:
-                    img = low_5_ll_img[ind] if ind < 5 else cond[ind-5]
-                    imgs.append(img.unsqueeze(0))
-                low_5_ll_img = torch.cat(imgs, dim=0)
+                if find_top_low_LL:
+                    top_5_ll, indices = torch.cat((top_5_ll, sample_ll), dim=0).sort(descending=True)
+                    top_5_ll = top_5_ll[:5]
+                    indices = indices[:5]
+                    imgs = []
+                    for ind in indices:
+                        img = top_5_ll_img[ind] if ind < 5 else cond[ind-5]
+                        imgs.append(img.unsqueeze(0))
+                        top_5_ll_img = torch.cat(imgs, dim=0)
 
-                if True:
-                    lls = [[f"{n:.2f}" for n in ll.tolist()] for ll in [top_5_ll, low_5_ll]]
-                    title = f"Samples of highest LL:\nLLs of sampled center boxes: [{', '.join(lls[0])}]"
-                    plot_img(top_5_ll_img, title)
-                    title = f"Samples of lowest LL:\nLLs of sampled center boxes: [{', '.join(lls[1])}]"
-                    plot_img(low_5_ll_img, title)
+                    low_5_ll, indices = torch.cat((low_5_ll, sample_ll), dim=0).sort(descending=False)
+                    low_5_ll = low_5_ll[:5]
+                    indices = indices[:5]
+                    imgs = []
+                    for ind in indices:
+                        img = low_5_ll_img[ind] if ind < 5 else cond[ind-5]
+                        imgs.append(img.unsqueeze(0))
+                        low_5_ll_img = torch.cat(imgs, dim=0)
+
+                    if True:
+                        lls = [[f"{n:.2f}" for n in ll.tolist()] for ll in [top_5_ll, low_5_ll]]
+                        title = f"Samples of highest LL:\nLLs of sampled center boxes: [{', '.join(lls[0])}]"
+                        plot_img(top_5_ll_img, title)
+                        title = f"Samples of lowest LL:\nLLs of sampled center boxes: [{', '.join(lls[1])}]"
+                        plot_img(low_5_ll_img, title)
                     print("Set breakpoint here")
 
-            if show_all:
-                lls = [[f"{n:.2f}" for n in ll.tolist()] for ll in [data_ll, sample_ll]]
-                title = f"LLs of original center boxes: [{', '.join(lls[0])}]\n" \
-                        f"LLs of sampled center boxes: [{', '.join(lls[1])}]"
-                plot_img(sample, title, path=os.path.join(results_dir, f"inspect{i+1}.png"))
+                if show_all:
+                    lls = [[f"{n:.2f}" for n in ll.tolist()] for ll in [sample_ll]]
+                    title = f"LLs of samples: [{', '.join(lls[0][:5])},\n{', '.join(lls[0][5:])}]"
+                    plot_img(sample, title, path=None)
                 print("Set breakpoint here")
-
-            print(i)
-            if i > 9:
-                exit()
         exit()
 
     # Construct Cspn from config
     train_loader, test_loader = get_mnist_loaders(args.dataset_dir, use_cuda, batch_size=batch_size, device=device)
-    config = CspnConfig()
+    if args.ratspn:
+        config = RatSpnConfig()
+        config.C = 10
+    else:
+        config = CspnConfig()
+        config.F_cond = (cond_size,)
+        config.C = 1
+        config.nr_feat_layers = args.nr_feat_layers
+        config.fc_sum_param_layers = args.nr_sum_param_layers
+        config.fc_dist_param_layers = args.nr_dist_param_layers
     config.F = int(np.prod(img_size))
-    config.F_cond = (cond_size,)
     config.R = args.repetitions
     config.D = args.cspn_depth
     config.I = args.num_dist
     config.S = args.num_sums
-    config.C = 1
     config.dropout = args.dropout
     config.leaf_base_class = RatNormal
     config.leaf_base_kwargs = {'min_sigma': 0.1, 'max_sigma': 1.0, 'min_mean': 0.0, 'max_mean': 1.0}
     config.first_layer_sum = args.sumfirst
 
-    config.nr_feat_layers = args.nr_feat_layers
-    config.fc_sum_param_layers = args.nr_sum_param_layers
-    config.fc_dist_param_layers = args.nr_dist_param_layers
-
     print("Using device:", device)
     print("Config:", config)
-    model = CSPN(config)
+    if args.ratspn:
+        model = RatSpn(config)
+        count_params(model)
+    else:
+        model = CSPN(config)
+        print_cspn_params(model)
     model = model.to(device)
     model.train()
     print(model)
-    print_cspn_params(model)
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
+    lmbda = 1.0
     sample_interval = 1 if args.verbose else 10  # number of epochs
     for epoch in range(args.epochs):
+        if epoch > 20:
+            lmbda = 0.5
         t_start = time.time()
         running_loss = []
         running_ent = []
@@ -307,7 +334,6 @@ if __name__ == "__main__":
         for batch_index, (image, label) in enumerate(train_loader):
             # Send data to correct device
             image = image.to(device)
-            label = F.one_hot(label, cond_size).float().to(device)
             # plt.imshow(data[0].permute(1, 2, 0))
             # plt.show()
 
@@ -316,8 +342,16 @@ if __name__ == "__main__":
             # model.entropy_lb(cond)
             optimizer.zero_grad()
             data = image.reshape(image.shape[0], -1)
-            output: torch.Tensor = model(x=data, condition=label)
-            loss = -output.mean()
+            if args.ratspn:
+                output: torch.Tensor = model(x=data)
+                loss_ce = F.cross_entropy(output, label)
+                loss_nll = -output.mean()
+                loss = (1 - lmbda) * loss_nll + lmbda * loss_ce
+            else:
+                label = F.one_hot(label, cond_size).float().to(device)
+                output: torch.Tensor = model(x=data, condition=label)
+                loss = -output.mean()
+
             loss.backward()
             optimizer.step()
             running_loss.append(loss.item())
