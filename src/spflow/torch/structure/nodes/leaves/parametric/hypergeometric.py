f8847ffe8e637ed35b48222668c22ea441beb76c
Philipp Deibert <pdeibert@pm.me>
2022-08-07 22:35:33 +0200

    Add missing imports

--- src/spflow/torch/structure/nodes/leaves/parametric/hypergeometric.py
+++ src/spflow/torch/structure/nodes/leaves/parametric/hypergeometric.py
@@ -5,7 +5,7 @@ Created on November 06, 2021
 """
 import numpy as np
 import torch
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from spflow.meta.scope.scope import Scope
 from spflow.meta.dispatch.dispatch import dispatch
 from spflow.meta.contexts.dispatch_context import DispatchContext
