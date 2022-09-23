efb34fe814b2cc9aac710114076d6606585afa36
Philipp Deibert <pdeibert@pm.me>
2022-09-23 16:28:52 +0200

    LearnSPN for base backend

---
+++ src/spflow/base/learning/learn_spn/learn_spn.py
@@ -0,0 +1,159 @@
+import numpy as np
+from functools import partial
+from typing import Callable, Union, Optional, Dict, Any
+from spflow.meta.scope.scope import Scope
+from spflow.base.learning.learn_spn.rdc import randomized_dependency_coefficients
+from spflow.base.learning.nodes.leaves.parametric.gaussian import maximum_likelihood_estimation
+from spflow.base.structure.nodes.leaves.parametric.gaussian import Gaussian
+from spflow.base.structure.nodes.node import SPNSumNode, SPNProductNode
+from networkx.algorithms.components.connected import connected_components
+from networkx.convert_matrix import from_numpy_array
+from sklearn.cluster import KMeans
+
+
+def partition_by_rdc(data, threshold=0.3, preprocessing: Optional[Callable]=None):
+
+    # perform optional pre-processing of data
+    if preprocessing is not None:
+        partitioning_data = preprocessing(data)
+    else:
+        partitioning_data = data
+
+    # get pairwise rdc values
+    rdcs = randomized_dependency_coefficients(partitioning_data)
+
+    # create adjacency matrix of features from thresholded rdcs
+    adj_mat = (rdcs >= threshold).astype(int)
+    
+    return [list(cc) for cc in connected_components(from_numpy_array(adj_mat))]
+
+
+def cluster_by_kmeans(data, n_clusters=2, preprocessing: Optional[Callable]=None):
+
+    # perform optional pre-processing of data
+    if preprocessing is not None:
+        clustering_data = preprocessing(data)
+    else:
+        clustering_data = data
+    
+    # compute k-Means clusters
+    data_labels = KMeans(n_clusters=n_clusters).fit_predict(clustering_data)
+
+    return data_labels
+
+
+def learn_spn(data, scope: Optional[Scope]=None, min_features_slice: int=2, min_instances_slice: int=100, fit_leaves: bool=True, clustering_method: Union[str, Callable]="kmeans", partitioning_method: Union[str, Callable]="rdc", clustering_args: Optional[Dict[str, Any]]=None, partitioning_args: Optional[Dict[str, Any]]=None):
+
+    # initialize scope
+    if scope is None:
+        scope = Scope(list(range(data.shape[1])))
+    else:
+        if len(scope.evidence) != 0:
+            raise ValueError("Scope specified for 'learn_spn' may not contain evidence variables.")
+        if len(scope.query) != data.shape[1]:
+            raise ValueError(f"Number of query variables in 'scope' does not match number of features in data.")
+
+    # available off-the-shelf clustering methods provided by SPFlow
+    if isinstance(clustering_method, str):
+        # Randomized Dependence Coefficients (RDCs)
+        if clustering_method == "kmeans":
+            clustering_method = cluster_by_kmeans
+        else:
+            raise ValueError(f"Value '{clustering_method}' for partitioning method is invalid.")
+    
+    # available off-the-shelf partitioning methods provided by SPFlow
+    if isinstance(partitioning_method, str):
+        # Randomized Dependence Coefficients (RDCs)
+        if partitioning_method == "rdc":
+            partitioning_method = partition_by_rdc
+        else:
+            raise ValueError(f"Value '{partitioning_method}' for partitioning method is invalid.")
+    
+    # for convenience, directly bind additional keyword arguments to the methods
+    if clustering_args is not None:
+        clustering_method = partial(clustering_method, **clustering_args)
+    if partitioning_args is not None:
+        partitioning_method = partial(partitioning_method, **partitioning_args)
+
+    if not isinstance(min_instances_slice, int) or min_instances_slice < 2:
+        raise ValueError(f"Value for 'min_instances_slice' must be an integer greater than 1, but was: {min_instances_slice}.")
+    if not isinstance(min_features_slice, int) or min_instances_slice < 2:
+        raise ValueError(f"Value for 'min_features_slice' must be an integer greater than 1, but was: {min_features_slice}.")
+
+    # helper functions
+    def create_uv_leaf(scope: Scope, data: np.ndarray, fit_leaves: bool=True):
+        # create leaf node
+        leaf = Gaussian(scope=scope) # TODO: infer correct leaf node
+
+        if fit_leaves:
+            # estimate leaf node parameters from data
+            maximum_likelihood_estimation(leaf, data)
+        
+        return leaf
+
+    def create_partitioned_mv_leaf(scope: Scope, data: np.ndarray, fit_leaves: bool=True):
+        # combine univariate leafs via product node
+        leaves = []
+
+        for rv in scope.query:
+            # create leaf node
+            leaf = Gaussian(scope=Scope([rv]))
+            leaves.append(leaf)
+
+            if fit_leaves:
+                # estimate leaf node parameters from data
+                maximum_likelihood_estimation(leaf, data[:, [rv]])
+
+        return SPNProductNode(children=leaves)
+
+    # TODO: remove irrelevant features?
+
+    # features does not need to be split any further
+    if len(scope.query) < min_features_slice:
+
+        # multivariate scope
+        if len(scope.query) > 1:
+            return create_partitioned_mv_leaf(scope, data, fit_leaves)
+        # univariate scope
+        else:
+            return create_uv_leaf(scope, data, fit_leaves)
+    else:
+        # select correct data
+        partitions = partitioning_method(data)
+
+        # multiple partition (i.e., data can be partitioned)
+        if len(partitions) > 1:
+            return SPNProductNode(
+                children=[
+                    # compute child trees recursively
+                    learn_spn(data[:, partition],
+                            scope=Scope([scope.query[rv] for rv in partition]),
+                            clustering_method=clustering_method,
+                            partitioning_method=partitioning_method,
+                            fit_leaves=fit_leaves,
+                            min_features_slice=min_features_slice,
+                            min_instances_slice=min_instances_slice
+                        ) for partition in partitions
+                ]
+            )
+        else:
+            # if not enough instances to cluster, create univariate leaf nodes (can be set to prevent overfitting too much or to reduce network size)
+            if data.shape[0] < min_instances_slice:
+                return create_partitioned_mv_leaf(scope, data, fit_leaves)
+            # cluster data
+            else:
+                labels = clustering_method(data)
+
+                return SPNSumNode(
+                    children=[
+                        learn_spn(data[labels == cluster_id, :],
+                                scope=scope,
+                                clustering_method=clustering_method,
+                                partitioning_method=partitioning_method,
+                                fit_leaves=fit_leaves,
+                                min_features_slice=min_features_slice,
+                                min_instances_slice=min_instances_slice
+                            ) for cluster_id in np.unique(labels)
+                    ],
+                    weights=[(labels == cluster_id).sum()/data.shape[0] for cluster_id in np.unique(labels)]
+                )
