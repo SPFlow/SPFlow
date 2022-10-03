66de7fb9cbba3ed20549d1ceb67800261c676fc5
Philipp Deibert <pdeibert@pm.me>
2022-10-03 18:32:57 +0200

    Refactor LearnSPN

---
+++ src/spflow/base/utils/connected_components.py
@@ -0,0 +1,48 @@
+"""
+Created on September 7, 2022
+
+@authors: Philipp Deibert
+"""
+import numpy as np
+
+
+def connected_components(adjacency_matrix: np.ndarray) -> np.ndarray:
+    """TODO."""
+    
+    if not np.all(adjacency_matrix == adjacency_matrix.T):
+        raise ValueError("Connected components expects input to be an undirected graph, but specified adjacency matrix was not symmetrical.")
+
+    # convert adjacency matrix to boolean (any non-zero entries are treated as an edge)
+    adjacency_matrix = (adjacency_matrix != 0)
+
+    # set of all vertices
+    vertices = set(range(adjacency_matrix.shape[0]))
+
+    # list of connected components
+    ccs = []
+
+    while vertices:
+
+        # perform breadth-first search
+        visited = set()
+        active_set = set([vertices.pop()])
+
+        # while there are previously unvisited vertices
+        while active_set:
+
+            source = active_set.pop()
+            visited.update([source])
+
+            # get neighbors of node
+            neighbors = np.where(adjacency_matrix[source])[0]
+
+            # add all unvisited vertices to active set
+            active_set.update(set(neighbors.tolist()).difference(visited))
+
+        # add visited vertices to list of connected components
+        ccs.append(visited)
+
+        # remove connected component from set of vertices to explore
+        vertices = vertices.difference(visited)
+
+    return ccs
