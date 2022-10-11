73ba53a96779307bd8d6a080bf1b314bd0e04a9e
Philipp Deibert <pdeibert@pm.me>
2022-10-11 13:39:58 +0200

    Warn user if 'log_likelihood' is dispatched without memoization

--- src/spflow/meta/dispatch/dispatch.py
+++ src/spflow/meta/dispatch/dispatch.py
@@ -3,6 +3,7 @@ Created on August 03, 2022
 
 @authors: Philipp Deibert
 """
+import warnings
 from typing import Callable, Any
 from functools import wraps
 from plum import dispatch as plum_dispatch
@@ -18,12 +19,15 @@ def dispatch(*args, memoize=False, swappable=True) -> Callable:
         if(swappable): _f = swappable_decorator(_f)
         if(memoize): _f = memoize_decorator(_f)
 
+        if f.__name__ == 'log_likelihood' and not memoize:
+            warnings.warn("Function 'log_likelihood' was dispatched without memoization, but is recommended to save redundant computations and to correctly compute gradients in some backends. Ignore this message if this is intended.")
+
         return plum_dispatch(_f) 
 
     if(len(args) == 1 and callable(args[0])):
         f = args[0]
         return dispatch_decorator(f)
     elif(len(args) > 0):
-        raise ValueError("'dipatch' decorator received unknown positional arguments. Try keyword arguments instead.")
+        raise ValueError("'dispatch' decorator received unknown positional arguments. Try keyword arguments instead.")
     else:
         return dispatch_decorator
