edcef8389c93fd2b8e37e85372abcc1adf390947
Philipp Deibert <pdeibert@pm.me>
2022-10-23 17:57:35 +0200

    Fix torch multivariate Gaussian sampling

--- src/spflow/torch/sampling/nodes/leaves/parametric/multivariate_gaussian.py
+++ src/spflow/torch/sampling/nodes/leaves/parametric/multivariate_gaussian.py
@@ -21,14 +21,17 @@ def sample(leaf: MultivariateGaussian, data: torch.Tensor, dispatch_ctx: Optiona
     if any([i >= data.shape[0] for i in sampling_ctx.instance_ids]):
         raise ValueError("Some instance ids are out of bounds for data tensor.")
 
-    nan_data = torch.isnan(data[:, leaf.scope.query])
+    # compute nan_mask for specified instances
+    instances_mask = torch.zeros(data.shape[0]).bool()
+    instances_mask[torch.tensor(sampling_ctx.instance_ids)] = True
+
+    nan_data = torch.isnan(data[torch.meshgrid(torch.where(instances_mask)[0], torch.tensor(leaf.scope.query), indexing='ij')])
 
     # group by scope rvs to sample
     for nan_mask in torch.unique(nan_data, dim=0):
 
-        cond_mask = ~nan_mask
-        cond_rvs = torch.where(cond_mask)[0]
-        non_cond_rvs = torch.where(~cond_mask)[0]
+        cond_rvs = torch.tensor(leaf.scope.query)[torch.where(~nan_mask)[0]] # ids for evidence RVs
+        non_cond_rvs = torch.tensor(leaf.scope.query)[torch.where(nan_mask)[0]] # RVs to be sampled
 
         # no 'NaN' values (nothing to sample)
         if(torch.sum(nan_mask) == 0):
@@ -52,6 +55,6 @@ def sample(leaf: MultivariateGaussian, data: torch.Tensor, dispatch_ctx: Optiona
             # get conditional covariance matrix
             cond_cov = leaf.cov[torch.meshgrid(cond_rvs, non_cond_rvs, indexing='ij')]
 
-            data[torch.meshgrid(sampling_ids, non_cond_rvs, indexing='ij')] = joint_samples[:, ~cond_mask] + ((data[torch.meshgrid(sampling_ids, cond_rvs, indexing='ij')]-joint_samples[:, cond_mask])@(marg_cov_inv@cond_cov))
+            data[torch.meshgrid(sampling_ids, non_cond_rvs, indexing='ij')] = joint_samples[:, nan_mask] + ((data[torch.meshgrid(sampling_ids, cond_rvs, indexing='ij')]-joint_samples[:, ~nan_mask])@(marg_cov_inv@cond_cov))
 
     return data
