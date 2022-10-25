87eb31d64b088487a4e44f190305685787b4c706
Philipp Deibert <pdeibert@pm.me>
2022-10-25 14:53:34 +0200

    Docstrings for abstract Module class

--- src/spflow/torch/structure/module.py
+++ src/spflow/torch/structure/module.py
@@ -1,9 +1,7 @@
-"""
-Created on June 10, 2021
+# -*- coding: utf-8 -*-
+"""Contains the abstract 'Module' class for SPFlow modules in the 'torch' backend.
 
-@authors: Philipp Deibert
-
-This file provides the abstract Module class for building graph structures with the PyTorch backend.
+All valid SPFlow modules in the 'torch' backend should inherit from this class or a subclass of it.
 """
 from abc import ABC, abstractmethod
 from typing import List, Tuple, Optional, Union
@@ -15,14 +13,30 @@ from spflow.meta.structure.module import MetaModule
 
 
 class Module(MetaModule, nn.Module, ABC):
-    """Abstract module class for building graph structures with the PyTorch backend.
+    """Abstract module class for building graph-based models in the 'torch' backend.
+
+    Methods:
+        children():
+            Iterator over all modules that are children to the module in a directed graph.
 
     Attributes:
-        children:
-            List of child modules to form a directed graph of modules.
+        n_out:
+            Integer indicating the number of outputs.
+        scopes_out:
+            List of scopes representing the output scopes.
     """
     def __init__(self, children: Optional[List["Module"]]=None) -> None:
+        """Initializes 'Module' object.
 
+        Initializes module by correctly setting its children.
+
+        Args:
+            children:
+                List of modules that are children to the module in a directed graph.
+
+        Raises:
+            ValueError: children of invalid type.
+        """
         super(Module, self).__init__()
 
         if children is None:
@@ -36,7 +50,19 @@ class Module(MetaModule, nn.Module, ABC):
             self.add_module("child_{}".format(i + 1), child)
 
     def input_to_output_ids(self, input_ids: Union[List[int], torch.Tensor]) -> Tuple[List[int], List[int]]:
+        """Translates input indices into corresponding child module indices and child module output indices.
 
+        For a given sequence of input indices (taking the inputs of all child modules into account), computes
+        the corresponding child module indices and child module output indices.
+
+        Args:
+            input_ids:
+                List of integers or one-dimensional NumPy array of integers specifying input indices to the module.
+
+        Returns:
+            A tuple of two lists of integers. The first list contains indices of child modules and the
+            second list contains the corresponding output indices of the respective child modules.
+        """
         if len(input_ids) == 0:
             input_ids = list(range(self.n_out))
         
@@ -64,19 +90,38 @@ class Module(MetaModule, nn.Module, ABC):
         return child_ids.tolist(), output_ids.tolist()
     
     @abstractmethod
-    def n_out(self):
+    def n_out(self) -> int:
+        """Returns the number of output for this module."""
+        pass
+
+    @abstractmethod
+    def scopes_out(self) -> List[Scope]:
+        """Returns the output scope this module represents."""
         pass
 
 
 class NestedModule(Module, ABC):
-    """Convenient module class for nesting non-terminal modules.
-    
+    """Convenient abstract module class for modules in the 'torch' backend that nest non-terminal modules.
+
+    Methods:
+        children():
+            Iterator over all modules that are children to the module in a directed graph.
+
     Attributes:
-        children:
-            List of child modules to form a directed graph of modules.
+        n_out:
+            Integer indicating the number of outputs.
+        scopes_out:
+            List of scopes representing the output scopes.
     """
     def __init__(self, children: Optional[List[Module]]=None, **kwargs) -> None:
-        """TODO"""
+        """Initializes 'NestedModule' object.
+
+        Initializes module by correctly setting its children.
+
+        Args:
+            children:
+                List of modules that are children to this module in a directed graph.
+        """
         if children is None:
             children = []
         
@@ -84,9 +129,16 @@ class NestedModule(Module, ABC):
         self.placeholders = []
 
     def create_placeholder(self, input_ids: List[int]) -> "Placeholder":
-        """Creates a placholder module that can be used for internal non-terminal modules.
-        
-        Also registers the placeholder internally.
+        """Creates an internal placeholder module.
+
+        Creates and registers a placeholder module that can be used for internal non-terminal modules.
+
+        Args:
+            input_ids:
+                List of integers specifying the input indices of the host module that this placeholder should represent internally.
+
+        Returns:
+            'Placeholder' object (to pass as child to internal non-terminal modules).
         """
         # create and register placeholder
         ph = self.Placeholder(host=self, input_ids=input_ids)
@@ -95,16 +147,48 @@ class NestedModule(Module, ABC):
         return ph
     
     def set_placeholders(self, f_name: str, inputs: torch.Tensor, dispatch_ctx: DispatchContext, overwrite=True) -> None:
-        """Fills the cache for all registered placeholder modules given specified input values."""
+        """Fills the cache for all registered placeholder modules given a function name and specified input values.
+
+        Args:
+            f_name:
+                String of the function name to set the cache of the placeholders for.
+            inputs:
+                NumPy array of all inputs. Inputs to be cached are selected based on input indices the placeholders represent. 
+            dispatch_ctx:
+                Dispatch context to use cache of.
+            overwrite:
+                Boolean indicating whether or not to overwrite potentially existing cached values.
+        """
         for ph in self.placeholders:
             # fill placeholder cache with specified input values
             dispatch_ctx.cache_value(f_name, ph, inputs[:, ph.input_ids], overwrite=overwrite)
 
 
     class Placeholder(Module):
-        """Placeholder module as an intermediary module between nested non-terminal modules and actual child modules."""
+        """Placeholder module as an intermediary module between nested non-terminal modules and actual child modules.
+
+        Since all non-terminal modules need their children to be specified at creation, internal non-terminal modules would
+        have to have the same children as the outer host module. This is not ideal, therefore placeholders can be used instead
+        that simply act as mediators between the actual host module's children and the internal non-terminal modules.
+        Furthermore, placeholders can be used to select parts of modules' outputs and use them internally in arbirary ways.
+
+        Attributes:
+            n_out:
+                Integer indicating the number of outputs (equal to the number of inputs it represents).
+            scopes_out:
+                List of scopes representing the output scopes (equal to the scopes of the inputs it represents).
+        """
         def __init__(self, host: Module, input_ids: List[int]) -> None:
-            
+            """Initializes 'Placeholder' object.
+
+            Initializes module by correctly setting its children.
+
+            Args:
+                host:
+                    Host module, that the placeholder is part of.
+                input_ids:
+                    List of integers specifying the input indices of the host module that this placeholder should represent internally.
+            """
             super().__init__()
 
             self.host = host
@@ -118,8 +202,20 @@ class NestedModule(Module, ABC):
             # compute scope for placeholder
             self.scopes_out = [child_scopes[i] for i in input_ids]
 
-        def input_to_output_ids(self, input_ids: List[int]) -> Tuple[int, int]:
+        def input_to_output_ids(self, input_ids: List[int]) -> Tuple[List[int], List[int]]:
+            """Translates input indices to the host module into corresponding child module indices and child module output indices.
 
+            For a given sequence of input indices to the host module (taking the inputs of all child modules into account), computes
+            the corresponding child module indices and child module output indices.
+
+            Args:
+                input_ids:
+                    List of integers or one-dimensional NumPy array of integers specifying input indices to the module.
+
+            Returns:
+                A tuple of two lists of integers. The first list contains indices of child modules and the
+                second list contains the corresponding output indices of the respective child modules.
+            """
             if len(input_ids) == 0:
                 input_ids = list(range(len(self.input_ids)))
             
@@ -127,4 +223,5 @@ class NestedModule(Module, ABC):
 
         @property
         def n_out(self) -> int:
+            """Returns the number of output for this module."""
             return len(self.input_ids)
