8da94e7c76a5df89f10ff2da641794b1eba6a348
AlexTUD19 <47852089+AlexTUD19@users.noreply.github.com>
2023-10-29 13:13:54 +0100

    model manager to save and load the model + tests

---
+++ src/spflow/tensorly/utils/model_manager.py
@@ -0,0 +1,10 @@
+import pickle
+from spflow.tensorly.structure.module import Module
+
+
+def save_model(model: Module, file: str):
+    pickle.dump(model, open(file, "wb"))
+
+
+def load_model(file: str) -> Module:
+    return pickle.load(open(file, "rb"))
