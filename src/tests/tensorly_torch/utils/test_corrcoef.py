09a318985b960d957494804f9949beca2135f87b
AlexTUD19 <47852089+AlexTUD19@users.noreply.github.com>
2023-10-29 20:25:13 +0100

    utils tests

--- src/tests/tensorly_torch/utils/test_corrcoef.py
+++ src/tests/tensorly_torch/utils/test_corrcoef.py
@@ -6,33 +6,27 @@ import torch
 
 from spflow.torch.utils import corrcoef
 
+tc = unittest.TestCase()
 
-class TestNode(unittest.TestCase):
-    @classmethod
-    def setup_class(cls):
-        torch.set_default_dtype(torch.float64)
+def test_corrcoef(do_for_all_backends):
+    if do_for_all_backends == "numpy":
+        return
 
-    @classmethod
-    def teardown_class(cls):
-        torch.set_default_dtype(torch.float32)
+    np.random.seed(0)
+    torch.manual_seed(0)
+    random.seed(0)
 
-    def test_corrcoef(self):
+    data = np.hstack(
+        [
+            np.random.multivariate_normal(np.array([-1.0, 3.0]), np.eye(2), (100,)),
+            np.random.randn(100, 1),
+        ]
+    )
 
-        np.random.seed(0)
-        torch.manual_seed(0)
-        random.seed(0)
+    corrcoef_np = np.corrcoef(data[:, [0, 1]].T, data[:, [2]].T)
+    corrcoef_torch = corrcoef(torch.tensor(data))
 
-        data = np.hstack(
-            [
-                np.random.multivariate_normal(np.array([-1.0, 3.0]), np.eye(2), (100,)),
-                np.random.randn(100, 1),
-            ]
-        )
-
-        corrcoef_np = np.corrcoef(data[:, [0, 1]].T, data[:, [2]].T)
-        corrcoef_torch = corrcoef(torch.tensor(data))
-
-        self.assertTrue(np.allclose(corrcoef_np, corrcoef_torch.numpy()))
+    tc.assertTrue(np.allclose(corrcoef_np, corrcoef_torch.numpy()))
 
 
 if __name__ == "__main__":
--- src/tests/tensorly_torch/utils/test_empirical_cdf.py
+++ src/tests/tensorly_torch/utils/test_empirical_cdf.py
@@ -1,21 +1,24 @@
 import unittest
 
 import torch
+import tensorly as tl
+import numpy as np
 
-from spflow.torch.utils import empirical_cdf
+from spflow.tensorly.utils import empirical_cdf
 
+tc = unittest.TestCase()
 
-class TestNode(unittest.TestCase):
-    def test_empirical_cdf(self):
+def test_empirical_cdf(do_for_all_backends):
 
-        data = torch.tensor([[0.1, 0.3], [0.5, -1.0], [0.1, 0.2], [0.0, -1.0]])
+    data = tl.tensor([[0.1, 0.3], [0.5, -1.0], [0.1, 0.2], [0.0, -1.0]])
 
-        # actual ecdf values
-        target_ecdf = torch.tensor([[3 / 4, 4 / 4], [4 / 4, 2 / 4], [3 / 4, 3 / 4], [1 / 4, 2 / 4]])
+    # actual ecdf values
+    target_ecdf = tl.tensor([[3 / 4, 4 / 4], [4 / 4, 2 / 4], [3 / 4, 3 / 4], [1 / 4, 2 / 4]])
 
-        ecdf = empirical_cdf(data)
-        self.assertTrue(torch.allclose(ecdf, target_ecdf))
+    ecdf = empirical_cdf(data)
+    tc.assertTrue(np.allclose(ecdf, target_ecdf))
 
 
 if __name__ == "__main__":
+    torch.set_default_dtype(torch.float64)
     unittest.main()
