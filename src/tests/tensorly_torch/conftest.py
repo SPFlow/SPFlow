de0166aacd05184655748e1c5ca8844bfd446ff7
AlexTUD19 <47852089+AlexTUD19@users.noreply.github.com>
2023-11-04 18:35:43 +0100

    conftest.py clean up

--- src/tests/tensorly_torch/conftest.py
+++ src/tests/tensorly_torch/conftest.py
@@ -3,17 +3,12 @@ from typing import Callable
 import tensorly as tl
 from pytest import fixture
 
-
-CONFIG_VALUE = "pytorch"
-
-
-
 @fixture(
-    scope="function", # function?
+    scope="function",
     autouse=True,
-    params=["numpy", "pytorch"], # test only numpy
+    params=["numpy", "pytorch"],
 )
 def do_for_all_backends(request) -> Callable:
     backend_name = request.param
     with tl.backend_context(backend_name):
-        yield backend_name  # we would not have to yield anything useful, could also be None
+        yield backend_name
