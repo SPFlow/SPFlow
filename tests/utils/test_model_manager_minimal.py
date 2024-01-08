d8853bfee98dd122b4f749372b40e7025a583a71
Felix Divo <felix.divo@cs.tu-darmstadt.de>
2024-01-08 14:16:18 +0000

    Attempt to increase the test coverage

---
+++ tests/utils/test_model_manager_minimal.py
@@ -0,0 +1,39 @@
+"""TODO: A dummy test case that shall be replaced by the proper one in `tests_old`."""
+
+import unittest
+
+from pytest import fixture
+
+from spflow.meta.data import Scope
+from spflow.modules.node import Node, ProductNode
+from spflow.modules.node.leaf import Gaussian
+from spflow.utils.model_manager import load_model, save_model
+from ..fixtures import backend_auto
+
+
+@fixture
+def example_spn() -> Node:
+    return ProductNode(
+        children=[
+            Gaussian(Scope([0])),
+        ],
+    )
+
+
+@fixture
+def tmp_model_file(tmp_path):
+    return tmp_path / "test_model.pkl"
+
+
+def test_save_model(backend_auto, tmp_model_file, example_spn):
+    save_model(example_spn, tmp_model_file)
+    assert tmp_model_file.exists()
+
+
+def test_load_model(backend_auto, tmp_model_file, example_spn):
+    save_model(example_spn, tmp_model_file)
+    load_model(tmp_model_file)
+
+
+if __name__ == "__main__":
+    unittest.main()
