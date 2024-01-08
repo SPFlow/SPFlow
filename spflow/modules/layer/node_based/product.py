51094e3ef90b15e1f501eeb6bd71b3bdf589da61
Steven Braun <steven.braun.mz@gmail.com>
2024-01-08 10:41:27 +0100

    Remove wrong imports

--- spflow/modules/layer/node_based/product.py
+++ spflow/modules/layer/node_based/product.py
@@ -4,12 +4,10 @@ from copy import deepcopy
 from typing import List, Optional, Union
 from collections.abc import Iterable
 
-import tensorly as tl
-import torch
-
 from spflow.meta.dispatch import SamplingContext, init_default_sampling_context
 from spflow.modules.module import Module
-from spflow.tensor.ops import Tensor
+from spflow.tensor import Tensor
+from spflow import tensor as T
 from spflow.modules.nested_module import NestedModule
 from spflow.modules.node import ProductNode
 
