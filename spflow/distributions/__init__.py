67788e7afa06557449a94394d5552dae5cfb1bdc
Steven Braun <steven.braun.mz@gmail.com>
2024-02-09 16:47:34 +0100

    Add new leaf class hierarchy

---
+++ spflow/distributions/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+from .normal import Normal
+from .distribution import Distribution
--- tests/modules/node/leaf/utils.py
+++ tests/modules/node/leaf/utils.py
@@ -1,8 +1,9 @@
 from spflow import log_likelihood
 import torch
+from spflow.modules.leaf_module import LeafModule
 
 
-def evaluate_log_likelihood(node, data):
+def evaluate_log_likelihood(node: LeafModule, data: torch.Tensor):
     lls = log_likelihood(node, data, check_support=True)
-    assert lls.shape == (data.shape[0], 1)
+    assert lls.shape == (data.shape[0], len(node.scope.query), node.event_shape[1])
     assert torch.isfinite(lls).all()
