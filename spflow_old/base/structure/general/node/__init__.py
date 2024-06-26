f53d2121d92ff5ef98988296fee93f1724e5ea86
Steven Braun <steven.braun.mz@gmail.com>
2024-06-26 12:10:08 +0200

    Remove `spflow_old` dir
    Code is still available prior to the restructuring in the `development` branch.

--- spflow_old/base/structure/general/node/__init__.py
+++
@@ -1,6 +0,0 @@
-# ---- sub-packages -----
-# from . import leaves
-# from .leaf_node import LeafNode
-
-# ----- specific imports -----
-# from .node import Node
--- spflow_old/torch/structure/general/__init__.py
+++
@@ -1,2 +0,0 @@
-# ---- sub-packages -----
-from . import layers, nodes
