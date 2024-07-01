702b037209826932fed5b2bc54070f0fb00afab1
Steven Braun <steven.braun.mz@gmail.com>
2024-07-01 18:04:18 +0200

    Rework product modules into Product/Cross/Elementwise

---
+++ spflow/modules/cross_product.py
@@ -0,0 +1,97 @@
+from typing import Optional, Union
+
+import torch
+from torch import Tensor
+
+from spflow.meta.dispatch.dispatch import dispatch
+from spflow.meta.dispatch.dispatch_context import (
+    DispatchContext,
+    init_default_dispatch_context,
+)
+from spflow.modules.base_product import BaseProduct, _get_input_log_likelihoods
+from spflow.modules.module import Module
+
+
+class CrossProduct(BaseProduct):
+    def __init__(
+        self,
+        inputs: Union[Module, tuple[Module, Module], list[Module]],
+        split_method: Optional[str] = None,
+        split_indices: Optional[
+            Union[tuple[list[int], list[int]], torch.IntTensor, tuple[torch.IntTensor, torch.IntTensor]]
+        ] = None,
+    ) -> None:
+        r"""Initializes ``CrossProduct`` module.
+
+        Args:
+            inputs:
+                Can be either a Module or a list of Modules.
+                The scopes for all child modules need to be pair-wise disjoint.
+
+                (1) If `inputs` is a list of Modules, they have to be of disjoint scopes and have equal number of features or a single feature wich will the be broadcast.
+
+                Example shapes:
+                    inputs = ((3, 4), (3, 5))
+                    output = (3, 4 * 5)
+
+                    inputs = ((3, 4), (3, 1))
+                    output = (3, 4 * 1)  # broadcasted
+
+                    inputs = ((3, 4), (1, 5))
+                    output = (3, 4 * 5)  # broadcasted
+
+                (2) If `inputs` is a single Module, the input is split into two equal-sized subsets. This can be either done by specifying the `split_indices` argument or by specifying the `split_method` argument.
+
+                Example shapes:
+                    inputs = (4, 3), split_method = "split_indices", split_indices = ([0, 1], [2, 3])
+                    splits = [(2, 3), (2, 3)]
+                    output = (2, 3 * 3)
+
+                    inputs = (4, 3), split_method = "random", split_indices = None
+                    splits = [(2, 3), (2, 3)]  # random split
+                    output = (2, 3 * 3)
+
+            split_method:
+                Method to split the input into two equal-sized subsets.
+                Possible values are "split_indices" and "random".
+                Defaults to "split_indices".
+
+            split_indices:
+                Indices to split the input into two equal-sized subsets.
+                If split_method is set to "split_indices", this argument is required.
+                Defaults to None.
+
+        Raises:
+            ValueError: Invalid arguments.
+        """
+        super().__init__(inputs=inputs, split_method=split_method, split_indices=split_indices)
+
+    @property
+    def out_channels(self) -> int:
+        """Returns the number of output nodes for this module."""
+        if self.has_single_input:
+            return self.inputs.out_channels**2
+        else:
+            return self.inputs[0].out_channels * self.inputs[1].out_channels
+
+
+@dispatch(memoize=True)  # type: ignore
+def log_likelihood(
+    module: CrossProduct,
+    data: Tensor,
+    check_support: bool = True,
+    dispatch_ctx: Optional[DispatchContext] = None,
+) -> Tensor:
+    # initialize dispatch context
+    dispatch_ctx = init_default_dispatch_context(dispatch_ctx)
+
+    lls = _get_input_log_likelihoods(module, data, check_support, dispatch_ctx)
+
+    # Compute the outer sum of left and right log-likelihoods
+    # [b, n, m1] + [b, n, m2] -> [b, n, m1, 1] + [b, n, 1, m2]  -> [b, n, m1, m2]
+    output = lls[0].unsqueeze(2) + lls[1].unsqueeze(3)
+
+    # View as [b, n, m1 * m2]
+    output = output.view(output.size(0), module.out_features, module.out_channels)
+
+    return output
---
+++ spflow/modules/product.py
@@ -0,0 +1,114 @@
+from typing import Optional, Union
+
+import torch
+from torch import Tensor
+
+from spflow.meta.dispatch import SamplingContext, init_default_sampling_context
+from spflow.meta.dispatch.dispatch import dispatch
+from spflow.meta.dispatch.dispatch_context import (
+    DispatchContext,
+    init_default_dispatch_context,
+)
+from spflow.modules.module import Module
+
+
+class Product(Module):
+    def __init__(self, inputs: Module) -> None:
+        super().__init__()
+        self.inputs = inputs
+
+        # Scope of this product module is equal to the scope of its only input
+        self.scope = inputs.scope
+
+    @property
+    def out_channels(self) -> int:
+        return self.inputs.out_channels
+
+    @property
+    def out_features(self) -> int:
+        return 1
+
+
+@dispatch(memoize=True)  # type: ignore
+def marginalize(
+    layer: Product,
+    marg_rvs: list[int],
+    prune: bool = True,
+    dispatch_ctx: Optional[DispatchContext] = None,
+) -> Union[Product, Module, None]:
+    # initialize dispatch context
+    dispatch_ctx = init_default_dispatch_context(dispatch_ctx)
+
+    # compute layer scope (same for all outputs)
+    layer_scope = layer.scope
+    marg_child = None
+    mutual_rvs = set(layer_scope.query).intersection(set(marg_rvs))
+
+    # layer scope is being fully marginalized over
+    if len(mutual_rvs) == len(layer_scope.query):
+        # passing this loop means marginalizing over the whole scope of this branch
+        pass
+    # node scope is being partially marginalized
+    elif mutual_rvs:
+        # marginalize child modules
+        marg_child_layer = marginalize(layer.inputs, marg_rvs, prune=prune, dispatch_ctx=dispatch_ctx)
+
+        # if marginalized child is not None
+        if marg_child_layer:
+            marg_child = marg_child_layer
+
+    else:
+        marg_child = layer.inputs
+
+    if marg_child is None:
+        return None
+
+    # ToDo: check if this is correct / prune if only one scope is left?
+    elif prune and marg_child.out_features == 1:
+        return marg_child
+    else:
+        return Product(inputs=marg_child)
+
+
+@dispatch  # type: ignore
+def sample(
+    product_layer: Product,
+    data: Tensor,
+    check_support: bool = True,
+    dispatch_ctx: Optional[DispatchContext] = None,
+    sampling_ctx: Optional[SamplingContext] = None,
+) -> Tensor:
+    # initialize contexts
+    dispatch_ctx = init_default_dispatch_context(dispatch_ctx)
+    sampling_ctx = init_default_sampling_context(sampling_ctx, data.shape[0])
+
+    sample(
+        product_layer.inputs,
+        data,
+        check_support=check_support,
+        dispatch_ctx=dispatch_ctx,
+        sampling_ctx=sampling_ctx,
+    )
+    return data
+
+
+@dispatch(memoize=True)  # type: ignore
+def log_likelihood(
+    product_layer: Product,
+    data: Tensor,
+    check_support: bool = True,
+    dispatch_ctx: Optional[DispatchContext] = None,
+) -> Tensor:
+    # initialize dispatch context
+    dispatch_ctx = init_default_dispatch_context(dispatch_ctx)
+
+    # compute child log-likelihoods
+    ll = log_likelihood(
+        product_layer.inputs,
+        data,
+        check_support=check_support,
+        dispatch_ctx=dispatch_ctx,
+    )  # shape: (batch_size, inputs.num_scopes, inputs.num_outputs)
+
+    # multiply childen (sum in log-space)
+    return torch.sum(ll, dim=1, keepdim=True)  # shape: (batch_size, product_layer.num_outputs)
