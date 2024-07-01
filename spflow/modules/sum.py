1233f044cd376f35cd95648c84cecbcd1efdbcb2
Steven Braun <steven.braun.mz@gmail.com>
2024-07-01 18:06:55 +0200

    Rework sum module

---
+++ spflow/modules/sum.py
@@ -0,0 +1,338 @@
+from typing import Optional, Union
+
+import torch
+from torch import Tensor
+
+from spflow.exceptions import InvalidParameterCombinationError, ScopeError
+from spflow.meta.data import Scope
+from spflow.meta.dispatch import SamplingContext, init_default_sampling_context
+from spflow.meta.dispatch.dispatch import dispatch
+from spflow.meta.dispatch.dispatch_context import (
+    DispatchContext,
+    init_default_dispatch_context,
+)
+from spflow.modules.module import Module
+from spflow.modules.ops.stack import Stack
+from spflow.utils.projections import (
+    proj_convex_to_real,
+)
+
+
+class Sum(Module):
+    """
+    A sum module that represents the sum operation over inputs.
+
+    The sum module can be used to sum over the channel dimension of a single input or over the stacked inputs.
+    """
+
+    def __init__(
+        self,
+        inputs: Union[Module, list[Module]],
+        out_channels: Optional[int] = None,
+        weights: Optional[Tensor] = None,
+    ) -> None:
+        """
+        Create a Sum module.
+
+        Args:
+            inputs: Can be a single input or a list of inputs.
+                - If a single input is provided, the sum module will sum over the channel dimension of the input.
+                - If a list of inputs is provided, the sum will be performed over the stacked inputs.
+            out_channels: Optional number of output nodes for each sum, if weights are not given.
+            weights: Optional weights for the sum module. If not provided, weights will be initialized randomly.
+        """
+        super().__init__()
+
+        if not input:
+            raise ValueError("'Sum' requires at least one input to be specified.")
+
+        if weights is not None:
+            if out_channels is not None:
+                raise InvalidParameterCombinationError(
+                    f"Cannot specify both 'out_channels' and 'weights' for 'Sum' module."
+                )
+
+            out_channels = weights.shape[2]
+
+        if out_channels < 1:
+            raise ValueError(
+                f"Number of nodes for 'Sum' must be greater of equal to 1 but was {out_channels}."
+            )
+
+        # Flag to allow conditional code based on single/multiple inputs
+        self.has_single_input = not isinstance(inputs, list)
+
+        if self.has_single_input:
+            # Single input, sum over in_channel dimension
+            self.inputs = inputs
+            self.sum_dim = 1
+            self._out_features = self.inputs.out_features
+            self._in_channels = self.inputs.out_channels
+            self._out_channels = out_channels
+            self.weights_shape = (self._out_features, self._in_channels, self._out_channels)
+        else:
+            # Check, that all inputs have the same shape
+            if not all([module.out_features == inputs[0].out_features for module in inputs]):
+                raise ValueError("All inputs must have the same number of features.")
+
+            if not all([module.out_channels in (1, inputs[0].out_channels) for module in inputs]):
+                raise ValueError(
+                    "All inputs must have the same number of channels or 1 channel (in which case the "
+                    "operation is broadcast)."
+                )
+
+            # Check that all input modules have the same scope
+            if not Scope.all_equal([module.scope for module in inputs]):
+                raise ScopeError("All input modules must have the same scope.")
+
+            # Multiple inputs, stack and sum over stacked dimension
+            self.inputs = Stack(inputs=inputs)
+            self._out_features = self.inputs.out_features
+            self._in_channels = self.inputs.out_channels
+
+            # out_channels will be flattened and thus multiplied by the number of inputs
+            self._out_channels = out_channels * self._in_channels
+            self.sum_dim = 3
+            self.weights_shape = (self._out_features, self._in_channels, out_channels, len(inputs))
+
+        # compute scope
+        self.scope = self.inputs.scope
+
+        # If weights are not provided, initialize them randomly
+        if weights is None:
+            weights = (
+                # weights has shape (n_nodes, n_scopes, n_inputs) to prevent permutation at ll and sample
+                torch.rand(self.weights_shape)
+                + 1e-08
+            )  # avoid zeros
+
+            # Normalize
+            weights /= torch.sum(weights, axis=self.sum_dim, keepdims=True)
+
+        # Register unnormalized log-probabilities for weights as torch parameters
+        self.logits = torch.nn.Parameter()
+
+        # Initialize weights (which sets self.logits under the hood accordingly)
+        self.weights = weights
+
+    @property
+    def out_features(self) -> int:
+        return self._out_features
+
+    @property
+    def out_channels(self) -> int:
+        return self._out_channels
+
+    @property
+    def log_weights(self) -> Tensor:
+        """Returns the weights of all nodes as a two-dimensional PyTorch tensor."""
+        # project auxiliary weights onto weights that sum up to one
+        return torch.nn.functional.log_softmax(self.logits, dim=self.sum_dim)
+
+    @property
+    def weights(self) -> Tensor:
+        """Returns the weights of all nodes as a two-dimensional PyTorch tensor."""
+        # project auxiliary weights onto weights that sum up to one
+        return torch.nn.functional.softmax(self.logits, dim=self.sum_dim)
+
+    @weights.setter
+    def weights(
+        self,
+        values: Tensor,
+    ) -> None:
+        """
+        Set weights of all nodes.
+
+        Args:
+            values: Three-dimensional PyTorch tensor containing weights for each input and node.
+        """
+        if values.shape != self.weights_shape:
+            raise ValueError(f"Invalid shape for weights: {values.shape}.")
+        if not torch.all(values > 0):
+            raise ValueError("Weights for 'Sum' must be all positive.")
+        if not torch.allclose(torch.sum(values, dim=self.sum_dim), torch.tensor(1.0)):
+            raise ValueError("Weights for 'Sum' must sum up to one.")
+        self.logits.data = proj_convex_to_real(values)
+
+    @log_weights.setter
+    def log_weights(
+        self,
+        values: Tensor,
+    ) -> None:
+        """
+        Set weights of all nodes.
+
+        Args:
+            values: Three-dimensional PyTorch tensor containing weights for each input and node.
+        """
+        if values.shape != self.log_weights.shape:
+            raise ValueError(f"Invalid shape for weights: {values.shape}.")
+        self.logits.data = values
+
+    def extra_repr(self) -> str:
+        return f"{super().extra_repr()}, weights={self.weights_shape}"
+
+
+@dispatch(memoize=True)  # type: ignore
+def marginalize(
+    module: Sum,
+    marg_rvs: list[int],
+    prune: bool = True,
+    dispatch_ctx: Optional[DispatchContext] = None,
+) -> Union[None, Sum]:
+    # initialize dispatch context
+    dispatch_ctx = init_default_dispatch_context(dispatch_ctx)
+
+    # compute module scope (same for all outputs)
+    module_scope = module.scope
+    marg_child = None
+
+    # for idx,s in enumerate(module_scope):
+    mutual_rvs = set(module_scope.query).intersection(set(marg_rvs))
+    module_weights = module.weights
+
+    # module scope is being fully marginalized over
+    if len(mutual_rvs) == len(module_scope.query):
+        # passing this loop means marginalizing over the whole scope of this branch
+        pass
+
+    # node scope is being partially marginalized
+    elif mutual_rvs:
+        # marginalize child modules
+        marg_child_module = marginalize(module.inputs, marg_rvs, prune=prune, dispatch_ctx=dispatch_ctx)
+
+        # if marginalized child is not None
+        if marg_child_module:
+            marg_child = marg_child_module
+            indices = [module.scope.query.index(el) for el in list(mutual_rvs)]
+            mask = torch.ones_like(torch.tensor(module_scope.query), dtype=torch.bool)
+            mask[indices] = False
+            module_weights = module_weights[mask]
+
+    else:
+        marg_child = module.inputs
+
+    if marg_child is None:
+        return None
+
+    else:
+        if module.has_single_input:
+            return Sum(inputs=marg_child, weights=module_weights)
+        else:
+            return Sum(inputs=[inp for inp in marg_child.inputs], weights=module_weights)
+
+
+@dispatch  # type: ignore
+def sample(
+    module: Sum,
+    data: Tensor,
+    check_support: bool = True,
+    dispatch_ctx: Optional[DispatchContext] = None,
+    sampling_ctx: Optional[SamplingContext] = None,
+) -> Tensor:
+    # initialize contexts
+    dispatch_ctx = init_default_dispatch_context(dispatch_ctx)
+    sampling_ctx = init_default_sampling_context(sampling_ctx, data.shape[0])
+
+    # Index into the correct weight channels given by parent module
+    # (stay in logits space since Categorical distribution accepts logits directly)
+    if module.has_single_input:
+        logits = module.logits.unsqueeze(0).expand(sampling_ctx.output_ids.shape[0], -1, -1, -1)
+        idxs = sampling_ctx.output_ids[..., None, None]
+        idxs = idxs.expand(-1, -1, module._in_channels, -1)
+        logits = logits.gather(dim=3, index=idxs).squeeze(3)
+    else:
+        logits = module.logits.unsqueeze(0).expand(sampling_ctx.output_ids.shape[0], -1, -1, -1, -1)
+        logits = logits.view(-1, module.out_features, module.out_channels, logits.shape[-1])
+        idxs = sampling_ctx.output_ids[..., None, None]
+        idxs = idxs.expand(-1, -1, -1, logits.shape[-1])
+        logits = logits.gather(dim=2, index=idxs).squeeze(3).squeeze(2)
+
+    # Sample from categorical distribution defined by weights to obtain indices into input channels
+    sampling_ctx.output_ids = torch.distributions.Categorical(logits=logits).sample()
+
+    # Sample from input module
+    sample(
+        module.inputs,
+        data,
+        check_support=check_support,
+        dispatch_ctx=dispatch_ctx,
+        sampling_ctx=sampling_ctx,
+    )
+
+    return data
+
+
+@dispatch(memoize=True)  # type: ignore
+def log_likelihood(
+    module: Sum,
+    data: Tensor,
+    check_support: bool = True,
+    dispatch_ctx: Optional[DispatchContext] = None,
+) -> Tensor:
+    # initialize dispatch context
+    dispatch_ctx = init_default_dispatch_context(dispatch_ctx)
+
+    # Get input log-likelihoods
+    ll = log_likelihood(  # shape: (B, F, IC)
+        module.inputs,
+        data,
+        check_support=check_support,
+        dispatch_ctx=dispatch_ctx,
+    )
+
+    ll = ll.unsqueeze(3)  # shape: (B, F, IC, 1)
+
+    log_weights = module.log_weights.unsqueeze(0)  # shape: (1, F, IC, OC)
+
+    # Weighted log-likelihoods
+    weighted_lls = ll + log_weights  # shape: (B, F, IC, OC)
+
+    # Sum over input channels (sum_dim + 1 since here the batch dimension is the first dimension)
+    output = torch.logsumexp(weighted_lls, dim=module.sum_dim + 1)  # shape: (B, F, OC)
+
+    output = output.view(data.shape[0], module.out_features, module.out_channels)
+
+    return output
+
+
+@dispatch(memoize=True)  # type: ignore
+def em(
+    module: Sum,
+    data: Tensor,
+    check_support: bool = True,
+    dispatch_ctx: Optional[DispatchContext] = None,
+) -> None:
+    # initialize dispatch context
+    dispatch_ctx = init_default_dispatch_context(dispatch_ctx)
+
+    with torch.no_grad():
+        # ----- expectation step -----
+        input_lls = dispatch_ctx.cache["log_likelihood"][
+            module.inputs
+        ]  # shape: (batch_size, 1, num_scopes, num_nodes_child)
+        node_lls = dispatch_ctx.cache["log_likelihood"][module]
+
+        if module.has_single_input:
+            log_weights = module.log_weights.unsqueeze(0)
+            log_grads = torch.log(node_lls.grad).unsqueeze(2)
+            input_lls = input_lls.unsqueeze(-1)
+            node_lls = node_lls.unsqueeze(2)
+        else:
+            log_weights = module.log_weights.unsqueeze(0)
+            input_lls = input_lls.unsqueeze(3)
+
+            s = (node_lls.shape[0], module.out_features, module._in_channels, module.weights.shape[2], 1)
+            log_grads = torch.log(node_lls.grad).view(s)
+            node_lls = node_lls.view(s)
+
+        log_expectations = log_weights + log_grads + input_lls - node_lls
+        log_expectations = log_expectations.logsumexp(0)  # Sum over batch dimension
+        log_expectations = log_expectations.log_softmax(module.sum_dim)  # Normalize
+
+        # ----- maximization step -----
+        module.log_weights = log_expectations
+
+        # NOTE: since we explicitely override parameters in 'maximum_likelihood_estimation', we do not need to zero/None parameter gradients
+
+    em(module.inputs, data, check_support=check_support, dispatch_ctx=dispatch_ctx)
---
+++ tests/modules/test_sum.py
@@ -0,0 +1,238 @@
+from tests.fixtures import auto_set_test_seed
+import unittest
+
+from itertools import product
+
+from spflow.exceptions import InvalidParameterCombinationError, ScopeError
+from spflow.meta.data import Scope
+import pytest
+from spflow.meta.dispatch import init_default_sampling_context
+from spflow import log_likelihood, sample, marginalize
+from spflow.learn import expectation_maximization
+from spflow.learn import gradient_descent
+from spflow.modules import Sum
+from tests.utils.leaves import make_normal_leaf, make_normal_data
+import torch
+
+in_channels_values = [1, 4]
+out_channels_values = [1, 5]
+out_features_values = [1, 6]
+is_single_input_values = [True, False]
+params = list(product(in_channels_values, out_channels_values, out_features_values, is_single_input_values))
+
+
+def make_sum(in_channels=None, out_channels=None, out_features=None, weights=None, is_single_input=True):
+    if isinstance(weights, list):
+        weights = torch.tensor(weights)
+        if weights.dim() == 1:
+            weights = weights.unsqueeze(1).unsqueeze(2)
+        elif weights.dim() == 2:
+            weights = weights.unsqueeze(2)
+
+    if weights is not None:
+        out_features = weights.shape[0]
+
+    if is_single_input:
+        inputs = make_normal_leaf(out_features=out_features, out_channels=in_channels)
+    else:
+        inputs_a = make_normal_leaf(out_features=out_features, out_channels=in_channels)
+        inputs_b = make_normal_leaf(out_features=out_features, out_channels=in_channels)
+        inputs = [inputs_a, inputs_b]
+
+    return Sum(out_channels=out_channels, inputs=inputs, weights=weights)
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_log_likelihood(in_channels: int, out_channels: int, out_features: int, is_single_input: bool):
+    out_channels = 3
+    module = make_sum(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        out_features=out_features,
+        is_single_input=is_single_input,
+    )
+    data = make_normal_data(out_features=out_features)
+    lls = log_likelihood(module, data)
+    assert lls.shape == (data.shape[0], module.out_features, module.out_channels)
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_sample(in_channels: int, out_channels: int, out_features: int, is_single_input: bool):
+    n_samples = 100
+    module = make_sum(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        out_features=out_features,
+        is_single_input=is_single_input,
+    )
+    sampling_ctx = init_default_sampling_context(sampling_ctx=None, n=n_samples)
+    for i in range(module.out_channels):
+        data = torch.full((n_samples, module.out_features), torch.nan)
+        sampling_ctx.output_ids = torch.randint(
+            low=0, high=module.out_channels, size=(n_samples, module.out_features)
+        )
+        samples = sample(module, data, sampling_ctx=sampling_ctx)
+        assert samples.shape == data.shape
+        samples_query = samples[:, module.scope.query]
+        assert torch.isfinite(samples_query).all()
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_expectation_maximization(
+    in_channels: int, out_channels: int, out_features: int, is_single_input: bool
+):
+    module = make_sum(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        out_features=out_features,
+        is_single_input=is_single_input,
+    )
+    data = make_normal_data(out_features=out_features)
+    expectation_maximization(module, data, max_steps=10)
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_gradient_descent_optimization(
+    in_channels: int, out_channels: int, out_features: int, is_single_input: bool
+):
+    module = make_sum(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        out_features=out_features,
+        is_single_input=is_single_input,
+    )
+    data = make_normal_data(out_features=out_features, num_samples=20)
+    dataset = torch.utils.data.TensorDataset(data)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=10)
+    gradient_descent(module, data_loader, epochs=1)
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_weights(in_channels: int, out_channels: int, out_features: int, is_single_input: bool):
+    if is_single_input:
+        weights = torch.ones((out_features, in_channels, out_channels))
+        weights = weights / weights.sum(dim=1, keepdim=True)
+    else:
+        weights = torch.ones((out_features, in_channels, out_channels, 2))
+        weights = weights / weights.sum(dim=3, keepdim=True)
+
+    module = make_sum(
+        weights=weights,
+        in_channels=in_channels,
+        is_single_input=is_single_input,
+    )
+    assert torch.allclose(module.weights.sum(dim=module.sum_dim), torch.tensor(1.0))
+    assert torch.allclose(module.log_weights, module.weights.log())
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_invalid_weights_normalized(
+    in_channels: int, out_channels: int, out_features: int, is_single_input: bool
+):
+    weights = torch.rand((out_features, in_channels, out_channels))
+    with pytest.raises(ValueError):
+        make_sum(weights=weights, in_channels=in_channels)
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_invalid_weights_negative(
+    in_channels: int, out_channels: int, out_features: int, is_single_input: bool
+):
+    weights = torch.rand((out_features, in_channels, out_channels)) - 1.0
+    with pytest.raises(ValueError):
+        make_sum(weights=weights, in_channels=in_channels)
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_invalid_input_channels_mismatch(
+    in_channels: int, out_channels: int, out_features: int, is_single_input: bool
+):
+    input_a = make_normal_leaf(out_features=out_features, out_channels=in_channels + 1)
+    input_b = make_normal_leaf(out_features=out_features, out_channels=in_channels + 2)
+    with pytest.raises(ValueError):
+        Sum(out_channels=out_channels, inputs=[input_a, input_b])
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_invalid_input_features_mismatch(
+    in_channels: int, out_channels: int, out_features: int, is_single_input: bool
+):
+    input_a = make_normal_leaf(out_features=out_features + 1, out_channels=in_channels)
+    input_b = make_normal_leaf(out_features=out_features + 2, out_channels=in_channels)
+    with pytest.raises(ValueError):
+        Sum(out_channels=out_channels, inputs=[input_a, input_b])
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_invalid_specification_of_out_channels_and_weights(
+    in_channels: int, out_channels: int, out_features: int, is_single_input: bool
+):
+    with pytest.raises(ValueError):
+        weights = torch.rand((out_features, in_channels, out_channels))
+        weights = weights / weights.sum(dim=2, keepdim=True)
+        Sum(weights=weights, inputs=make_normal_leaf(out_features=out_features, out_channels=3))
+
+
+@pytest.mark.parametrize("in_channels,out_channels,out_features,is_single_input", params)
+def test_invalid_parameter_combination(
+    in_channels: int, out_channels: int, out_features: int, is_single_input: bool
+):
+    weights = torch.rand((out_features, in_channels, out_channels)) + 1.0
+    with pytest.raises(InvalidParameterCombinationError):
+        make_sum(weights=weights, out_channels=out_channels, in_channels=in_channels)
+
+
+@pytest.mark.parametrize(
+    "in_channels,out_channels,out_features",
+    product(in_channels_values, out_channels_values, out_features_values),
+)
+def test_same_scope_error(in_channels: int, out_channels: int, out_features: int):
+    with pytest.raises(ScopeError):
+        input_a = make_normal_leaf(scope=Scope(range(0, out_features)), out_channels=in_channels)
+        input_b = make_normal_leaf(
+            scope=Scope(range(out_features, out_features * 2)), out_channels=in_channels
+        )
+        with pytest.raises(ValueError):
+            Sum(out_channels=out_channels, inputs=[input_a, input_b])
+
+
+@pytest.mark.parametrize(
+    "prune,in_channels,out_channels,is_single_input,marg_rvs",
+    product(
+        [True, False],
+        in_channels_values,
+        out_channels_values,
+        is_single_input_values,
+        [[0], [1], [2], [0, 1], [1, 2], [0, 2], [0, 1, 2]],
+    ),
+)
+def test_marginalize(prune, in_channels: int, out_channels: int, is_single_input: bool, marg_rvs: list[int]):
+    out_features = 3
+    module = make_sum(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        out_features=out_features,
+        is_single_input=is_single_input,
+    )
+    weights_shape = module.weights.shape
+
+    # Marginalize scope
+    marginalized_module = marginalize(module, marg_rvs, prune=prune)
+
+    if len(marg_rvs) == out_features:
+        assert marginalized_module is None
+        return
+
+    # Scope query should not contain marginalized rv
+    assert len(set(marginalized_module.scope.query).intersection(marg_rvs)) == 0
+
+    # Weights num_scopes dimension should be reduced by len(marg_rv)
+    assert marginalized_module.weights.shape[0] == weights_shape[0] - len(marg_rvs)
+
+    # Check that all other dims stayed the same
+    for d in range(1, len(weights_shape)):
+        assert marginalized_module.weights.shape[d] == weights_shape[d]
+
+
+if __name__ == "__main__":
+    unittest.main()
