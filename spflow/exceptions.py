d3e1ef5548f16fd4936a0ae07513195b6a6deebe
Steven Braun <steven.braun.mz@gmail.com>
2024-07-01 17:53:07 +0200

    Add custom exceptions

---
+++ spflow/exceptions.py
@@ -0,0 +1,10 @@
+class InvalidParameterCombinationError(Exception):
+    """Raised when an invalid combination of parameters is provided."""
+
+    pass
+
+
+class ScopeError(Exception):
+    """Raised when an invalid scope is provided."""
+
+    pass
