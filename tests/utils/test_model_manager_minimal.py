c0e53003cb7f15834d40a872326a2881d82b945b
Steven Braun <steven.braun.mz@gmail.com>
2024-07-01 18:08:30 +0200

    Fix model manager test

--- tests/utils/test_model_manager_minimal.py
+++ tests/utils/test_model_manager_minimal.py
@@ -1,22 +1,18 @@
-"""TODO: A dummy test case that shall be replaced by the proper one in `tests_old`."""
-
+from tests.fixtures import auto_set_test_seed
 import unittest
 
 from pytest import fixture
 
 from spflow.meta.data import Scope
-from spflow.modules.node import Node, ProductNode
-from spflow.modules.node.leaf import Normal
+from spflow.modules import Product
+from spflow.modules.leaf import Normal
 from spflow.utils.model_manager import load_model, save_model
+import torch
 
 
 @fixture
-def example_spn() -> Node:
-    return ProductNode(
-        inputs=[
-            Normal(Scope([0])),
-        ],
-    )
+def example_model() -> Product:
+    return Product(inputs=Normal(Scope([0]), mean=torch.randn(1, 1), std=torch.rand(1, 1)))
 
 
 @fixture
@@ -24,13 +20,13 @@ def tmp_model_file(tmp_path):
     return tmp_path / "test_model.pkl"
 
 
-def test_save_model(tmp_model_file, example_spn):
-    save_model(example_spn, tmp_model_file)
+def test_save_model(tmp_model_file, example_model):
+    save_model(example_model, tmp_model_file)
     assert tmp_model_file.exists()
 
 
-def test_load_model(tmp_model_file, example_spn):
-    save_model(example_spn, tmp_model_file)
+def test_load_model(tmp_model_file, example_model):
+    save_model(example_model, tmp_model_file)
     load_model(tmp_model_file)
 
 
