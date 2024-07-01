4de97546bc0cedda84f6c0c363a2784abd442c14
Steven Braun <steven.braun.mz@gmail.com>
2024-07-01 18:03:41 +0200

    Rename set seed fixture

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -6,7 +6,7 @@ import torch
     scope="function",
     autouse=True,
 )
-def set_seed():
+def auto_set_test_seed():
     torch.manual_seed(0)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(0)
