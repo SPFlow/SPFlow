48621ffd58ce006af4d480b5b9d7cebe29f48163
Steven Braun <steven.braun.mz@gmail.com>
2024-07-02 12:36:25 +0200

    Remove duplicate tests

--- tests/modules/leaf/test_normal.py
+++ tests/modules/leaf/test_normal.py
@@ -38,15 +38,6 @@ def make_leaf(mean, std) -> Normal:
 
 
 @pytest.mark.parametrize("out_features,out_channels", product(out_features_values, out_channels_values))
-def test_constructor_valid_params(out_features: int, out_channels: int):
-    """Test the constructor of a Normal distribution with valid parameters."""
-    mean, std = make_params(out_features, out_channels)
-    leaf = make_leaf(mean=mean, std=std)
-    assert torch.isclose(leaf.distribution.mean, mean).all()
-    assert torch.isclose(leaf.distribution.std, std).all()
-
-
-@pytest.mark.parametrize("out_features,out_channels", product(out_features_values, out_channels_values))
 def test_constructor_negative_std(out_features: int, out_channels: int):
     """Test the constructor of a Normal distribution with negative std."""
     mean, std = make_params(out_features, out_channels)
@@ -60,22 +51,6 @@ def test_constructor_zero_std(out_features: int, out_channels: int):
     mean, std = make_params(out_features, out_channels)
     with pytest.raises(ValueError):
         make_leaf(mean=mean, std=0.0 * std)
-
-
-@pytest.mark.parametrize("out_features,out_channels", product(out_features_values, out_channels_values))
-def test_constructor_nan_mean(out_features: int, out_channels: int):
-    """Test the constructor of a Normal distribution with NaN mean."""
-    mean, std = make_params(out_features, out_channels)
-    with pytest.raises(ValueError):
-        make_leaf(mean=mean * torch.nan, std=std)
-
-
-@pytest.mark.parametrize("out_features,out_channels", product(out_features_values, out_channels_values))
-def test_constructor_nan_std(out_features: int, out_channels: int):
-    """Test the constructor of a Normal distribution with NaN std."""
-    mean, std = make_params(out_features, out_channels)
-    with pytest.raises(ValueError):
-        make_leaf(mean=mean, std=std * torch.nan)
 
 
 @pytest.mark.parametrize("out_features,out_channels", product(out_features_values, out_channels_values))
