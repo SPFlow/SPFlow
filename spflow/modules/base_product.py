b1b0db961214599f1c432d46ebdb752cdb8a74f9
Steven Braun <steven.braun.mz@gmail.com>
2024-07-03 16:48:12 +0200

    Update base_product docstring

--- spflow/modules/base_product.py
+++ spflow/modules/base_product.py
@@ -29,38 +29,10 @@ class BaseProduct(Module, ABC):
     ) -> None:
         r"""Initializes ``BaseProduct`` object.
 
-        TODO: update description to be general
-
         Args:
             inputs:
                 Can be either a Module or a list of Modules.
                 The scopes for all child modules need to be pair-wise disjoint.
-
-                (1) If `inputs` is a list of Modules, they have to be of disjoint scopes and have equal number of features or a single feature wich will the be broadcast and an equal number of channels or a single channel which will be broadcast.
-
-                Example shapes:
-                    inputs = ((3, 4), (3, 4))
-                    output = (3, 4)
-
-                    inputs = ((3, 4), (3, 1))
-                    output = (3, 4)  # broadcasted
-
-                    inputs = ((3, 4), (1, 4))
-                    output = (3, 4)  # broadcasted
-
-                    inputs = ((3, 1), (1, 4))
-                    output = (3, 4)  # broadcasted
-
-                (2) If `inputs` is a single Module, the input is split into two equal-sized subsets. This can be either done by specifying the `split_indices` argument or by specifying the `split_method` argument.
-
-                Example shapes:
-                    inputs = (4, 3), split_method = "split_indices", split_indices = ([0, 1], [2, 3])
-                    splits = [(2, 3), (2, 3)]
-                    output = (2, 3)
-
-                    inputs = (4, 3), split_method = "random", split_indices = None
-                    splits = [(2, 3), (2, 3)]  # random split
-                    output = (2, 3)
 
             split_method:
                 Method to split the input into two equal-sized subsets.
