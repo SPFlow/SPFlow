4d1e51839290c488d76e5ad5431081b5262def62
Steven Braun <steven.braun.mz@gmail.com>
2024-07-09 09:09:52 +0200

    Fix sum module

--- spflow/modules/sum.py
+++ spflow/modules/sum.py
@@ -1,7 +1,7 @@
 from typing import Optional, Union
 
 import torch
-from torch import Tensor
+from torch import Tensor, nn
 
 from spflow.exceptions import InvalidParameterCombinationError, ScopeError
 from spflow.meta.data import Scope
@@ -12,7 +12,6 @@ from spflow.meta.dispatch.dispatch_context import (
     init_default_dispatch_context,
 )
 from spflow.modules.module import Module
-from spflow.modules.ops.stack import Stack
 from spflow.utils.projections import (
     proj_convex_to_real,
 )
@@ -67,14 +66,16 @@ class Sum(Module):
             self.inputs = inputs
             self.sum_dim = 1
             self._out_features = self.inputs.out_features
-            self._in_channels = self.inputs.out_channels
-            self._out_channels = out_channels
-            self.weights_shape = (self._out_features, self._in_channels, self._out_channels)
+            self._in_channels_total = self.inputs.out_channels
+            self._out_channels_total = out_channels
+            self.weights_shape = (self._out_features, self._in_channels_total, self._out_channels_total)
+            self.scope = self.inputs.scope
         else:
-            # Check, that all inputs have the same shape
+            # Check, that all inputs have the same number of features
             if not all([module.out_features == inputs[0].out_features for module in inputs]):
                 raise ValueError("All inputs must have the same number of features.")
 
+            # Check, that all inputs have the same number of channels or 1 channel (broadcast)
             if not all(
                 [module.out_channels in (1, max(m.out_channels for m in inputs)) for module in inputs]
             ):
@@ -87,26 +88,36 @@ class Sum(Module):
             if not Scope.all_equal([module.scope for module in inputs]):
                 raise ScopeError("All input modules must have the same scope.")
 
+            self.scope = inputs[0].scope
+
             # Multiple inputs, stack and sum over stacked dimension
-            self.inputs = Stack(inputs=inputs)
-            self._out_features = self.inputs.out_features
-            self._in_channels = self.inputs.out_channels
+            self.inputs = nn.ModuleList(inputs)
+            self._out_features = self.inputs[0].out_features
+            self._num_sums = out_channels
 
             # out_channels will be flattened and thus multiplied by the number of inputs
-            self._out_channels = out_channels * self._in_channels
+            self._in_channels_per_input = max([module.out_channels for module in self.inputs])
+            self._out_channels_total = self._num_sums * self._in_channels_per_input
             self.sum_dim = 3
-            self.weights_shape = (self._out_features, self._in_channels, out_channels, len(inputs))
-
-            # Store unraveled in- and out-channel indices
-            self.register_buffer(
-                name="unraveled_channel_indices",
-                tensor=torch.tensor(
-                    [(i, j) for i in range(self.inputs.out_channels) for j in range(out_channels)]
-                ),
+            self.weights_shape = (
+                self._out_features,
+                self._in_channels_per_input,
+                self._num_sums,
+                len(inputs),
             )
 
-        # compute scope
-        self.scope = self.inputs.scope
+            # Store unraveled in- and out-channel indices
+            # E.g. for 2 inputs with 3 in_channels_per_input, the mapping should be:
+            # [   0  ,    1  ,    2  ,    3  ,    4  ,    5  ]
+            #     |       |       |       |       |       |
+            #     v       v       v       v       v       v
+            # [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
+            #
+            # This is necessary to map the output indices to the correct input indices
+            unraveled_channel_indices = torch.tensor(
+                [(i, j) for i in range(self._in_channels_per_input) for j in range(self._num_sums)]
+            )
+            self.register_buffer(name="unraveled_channel_indices", tensor=unraveled_channel_indices)
 
         # If weights are not provided, initialize them randomly
         if weights is None:
@@ -131,7 +142,7 @@ class Sum(Module):
 
     @property
     def out_channels(self) -> int:
-        return self._out_channels
+        return self._out_channels_total
 
     @property
     def log_weights(self) -> Tensor:
@@ -209,7 +220,15 @@ def marginalize(
     # node scope is being partially marginalized
     elif mutual_rvs:
         # marginalize input modules
-        marg_input = marginalize(module.inputs, marg_rvs, prune=prune, dispatch_ctx=dispatch_ctx)
+        if module.has_single_input:
+            marg_input = marginalize(module.inputs, marg_rvs, prune=prune, dispatch_ctx=dispatch_ctx)
+        else:
+            marg_input = [
+                marginalize(inp, marg_rvs, prune=prune, dispatch_ctx=dispatch_ctx) for inp in module.inputs
+            ]
+
+            if all(mi is None for mi in marg_input):
+                marg_input = None
 
         # if marginalized input is not None
         if marg_input:
@@ -228,7 +247,7 @@ def marginalize(
         if module.has_single_input:
             return Sum(inputs=marg_input, weights=module_weights)
         else:
-            return Sum(inputs=[inp for inp in marg_input.inputs], weights=module_weights)
+            return Sum(inputs=[inp for inp in marg_input], weights=module_weights)
 
 
 @dispatch  # type: ignore
@@ -247,9 +266,9 @@ def sample(
     # Index into the correct weight channels given by parent module
     # (stay in logits space since Categorical distribution accepts logits directly)
     if module.has_single_input:
-        logits = module.logits.unsqueeze(0).expand(sampling_ctx.output_ids.shape[0], -1, -1, -1)
-        idxs = sampling_ctx.output_ids[..., None, None]
-        idxs = idxs.expand(-1, -1, module._in_channels, -1)
+        logits = module.logits.unsqueeze(0).expand(sampling_ctx.channel_index.shape[0], -1, -1, -1)
+        idxs = sampling_ctx.channel_index[..., None, None]
+        idxs = idxs.expand(-1, -1, module._in_channels_total, -1)
         logits = logits.gather(dim=3, index=idxs).squeeze(3)
 
         if (
@@ -261,21 +280,19 @@ def sample(
             # Compute log posterior by reweighing logits with input lls
             log_prior = logits
             log_posterior = log_prior + input_lls
-            log_posterior = log_posterior.log_softmax(
-                dim=2
-            )  # TODO: Can we leave this out since Cat normalizes anyway?
+            log_posterior = log_posterior.log_softmax(dim=2)
             logits = log_posterior
 
         # Sample from categorical distribution defined by weights to obtain indices into input channels
         if is_mpe:
             # Take the argmax of the logits to obtain the most probable index
-            sampling_ctx.output_ids = torch.argmax(logits, dim=-1)
+            sampling_ctx.channel_index = torch.argmax(logits, dim=-1)
         else:
             # Sample from categorical distribution defined by weights to obtain indices into input channels
-            sampling_ctx.output_ids = torch.distributions.Categorical(logits=logits).sample()
+            sampling_ctx.channel_index = torch.distributions.Categorical(logits=logits).sample()
 
         # Sample from input module
-        samples = sample(
+        sample(
             module.inputs,
             data,
             check_support=check_support,
@@ -284,54 +301,81 @@ def sample(
         )
 
     else:
-        logits = module.logits.unsqueeze(0).expand(sampling_ctx.output_ids.shape[0], -1, -1, -1, -1)
-        logits = logits.view(-1, module.out_features, module.out_channels, logits.shape[-1])
-        idxs = sampling_ctx.output_ids[..., None, None]
-        idxs = idxs.expand(-1, -1, -1, logits.shape[-1])
-        logits = logits.gather(dim=2, index=idxs).squeeze(3).squeeze(2)
+        logits = module.logits.unsqueeze(0).expand(sampling_ctx.channel_index.shape[0], -1, -1, -1, -1)
+        oids_mapped = module.unraveled_channel_indices[sampling_ctx.channel_index]
 
-        oids_mapped = module.unraveled_channel_indices[sampling_ctx.output_ids]
         # Take the first element of the tuple (input_channel_idx, output_channel_idx)
         # This is the out_channels index for all inputs in the Stack module
-        oids_in_channels = oids_mapped[..., 0]
+        cids_in_channels_per_input = oids_mapped[..., 0]
+        oids_num_sums = oids_mapped[..., 1]
 
-        if (
-            "log_likelihood" in dispatch_ctx.cache
-            and dispatch_ctx.cache["log_likelihood"][module.inputs] is not None
-        ):
-            input_lls = dispatch_ctx.cache["log_likelihood"][module.inputs]
+        # Index weights with oids_num_sums (selects the correct output channel)
+        oids_num_sums = oids_num_sums[..., None, None, None].expand(
+            -1, -1, logits.shape[-3], -1, logits.shape[-1]
+        )
+        logits = logits.gather(dim=3, index=oids_num_sums).squeeze(3)
 
-            oids_in_channels_input_lls = (
-                oids_in_channels.unsqueeze(2).unsqueeze(3).expand(-1, -1, -1, input_lls.shape[3])
+        # Index logits with oids_in_channels_per_input to get the correct logits for each input
+        logits = logits.gather(
+            dim=2, index=cids_in_channels_per_input[..., None, None].expand(-1, -1, -1, logits.shape[-1])
+        ).squeeze(2)
+
+        if module.has_single_input:
+            if (
+                "log_likelihood" in dispatch_ctx.cache
+                and dispatch_ctx.cache["log_likelihood"][module.inputs] is not None
+            ):
+                input_lls = dispatch_ctx.cache["log_likelihood"][module.inputs]
+                is_conditional = True
+            else:
+                is_conditional = False
+
+        else:
+            if "log_likelihood" in dispatch_ctx.cache and all(
+                dispatch_ctx.cache["log_likelihood"][inp] is not None for inp in module.inputs
+            ):
+                input_lls = [dispatch_ctx.cache["log_likelihood"][inp] for inp in module.inputs]
+                input_lls = torch.stack(input_lls, dim=-1)
+                is_conditional = True
+            else:
+                is_conditional = False
+
+        if is_conditional:
+            cids_in_channels_input_lls = (
+                cids_in_channels_per_input.unsqueeze(2).unsqueeze(3).expand(-1, -1, -1, input_lls.shape[3])
             )
-            input_lls = input_lls.gather(dim=2, index=oids_in_channels_input_lls).squeeze(2)
+            input_lls = input_lls.gather(dim=2, index=cids_in_channels_input_lls).squeeze(2)
 
-            # Compute log posterior by reweighting logits with input lls
+            # Compute log posterior by reweighing logits with input lls
             log_prior = logits
             log_posterior = log_prior + input_lls
             log_posterior = log_posterior.log_softmax(dim=2)
             logits = log_posterior
 
-        # Sample from categorical distribution defined by weights to obtain indices into the Stack dimension
-        oids_stack = torch.distributions.Categorical(logits=logits).sample()
-        sampling_ctx.output_ids = oids_in_channels
+        # Sample/MPE from categorical distribution defined by weights to obtain indices into the Stack dimension
+        if is_mpe:
+            cids_stack = torch.argmax(logits, dim=-1)
+        else:
+            cids_stack = torch.distributions.Categorical(logits=logits).sample()
 
         # Sample from input module
-        samples = sample(
-            module.inputs,
-            data,
-            check_support=check_support,
-            dispatch_ctx=dispatch_ctx,
-            sampling_ctx=sampling_ctx,
-        )
+        sampling_ctx.channel_index = cids_in_channels_per_input
 
-        # Index into the correct stack dimension given by parent module
-        samples = samples.gather(dim=-1, index=oids_stack.unsqueeze(-1)).squeeze(-1)
+        for i, inp in enumerate(module.inputs):
+            # Update feature_mask
+            mask = sampling_ctx.mask & (cids_stack == i)
 
-    # Update only relevant samples
-    data[sampling_ctx.instance_ids[:, None], module.scope.query] = samples[
-        sampling_ctx.instance_ids[:, None], module.scope.query
-    ]
+            sampling_ctx_cpy = sampling_ctx.copy()
+            sampling_ctx_cpy.mask = mask
+
+            # Sample from input module
+            sample(
+                inp,
+                data,
+                check_support=check_support,
+                dispatch_ctx=dispatch_ctx,
+                sampling_ctx=sampling_ctx_cpy,
+            )
 
     return data
 
@@ -346,14 +390,34 @@ def log_likelihood(
     # initialize dispatch context
     dispatch_ctx = init_default_dispatch_context(dispatch_ctx)
 
-    # Get input log-likelihoods
-    ll = log_likelihood(  # shape: (B, F, IC)
-        module.inputs,
-        data,
-        check_support=check_support,
-        dispatch_ctx=dispatch_ctx,
-    )
+    if module.has_single_input:
+        ll = log_likelihood(
+            module.inputs,
+            data,
+            check_support=check_support,
+            dispatch_ctx=dispatch_ctx,
+        )
+    else:
+        # Get input log-likelihoods
+        lls = []
+        for inp in module.inputs:
+            ll = log_likelihood(
+                inp,
+                data,
+                check_support=check_support,
+                dispatch_ctx=dispatch_ctx,
+            )
 
+            # Prepare for broadcasting
+            if inp.out_channels == 1 and module._in_channels_per_input > 1:
+                ll = ll.expand(data.shape[0], module.out_features, module._in_channels_per_input)
+
+            lls.append(ll)
+
+        # Stack input log-likelihoods
+        ll = torch.stack(lls, dim=module.sum_dim)
+
+    # if module.has_single_input:
     ll = ll.unsqueeze(3)  # shape: (B, F, IC, 1)
 
     log_weights = module.log_weights.unsqueeze(0)  # shape: (1, F, IC, OC)
@@ -381,25 +445,39 @@ def em(
 
     with torch.no_grad():
         # ----- expectation step -----
-        input_lls = dispatch_ctx.cache["log_likelihood"][
-            module.inputs
-        ]  # shape: (batch_size, 1, num_scopes, num_nodes_child)
-        node_lls = dispatch_ctx.cache["log_likelihood"][module]
+
+        # Get input LLs
+        if module.has_single_input:
+            input_lls = dispatch_ctx.cache["log_likelihood"][
+                module.inputs
+            ]  # shape: (batch_size, 1, num_scopes, num_nodes_child)
+        else:
+            input_lls = [dispatch_ctx.cache["log_likelihood"][inp] for inp in module.inputs]
+            input_lls = torch.stack(input_lls, dim=3)
+
+        # Get module lls
+        module_lls = dispatch_ctx.cache["log_likelihood"][module]
 
         if module.has_single_input:
             log_weights = module.log_weights.unsqueeze(0)
-            log_grads = torch.log(node_lls.grad).unsqueeze(2)
+            log_grads = torch.log(module_lls.grad).unsqueeze(2)
             input_lls = input_lls.unsqueeze(-1)
-            node_lls = node_lls.unsqueeze(2)
+            module_lls = module_lls.unsqueeze(2)
         else:
             log_weights = module.log_weights.unsqueeze(0)
             input_lls = input_lls.unsqueeze(3)
 
-            s = (node_lls.shape[0], module.out_features, module._in_channels, module.weights.shape[2], 1)
-            log_grads = torch.log(node_lls.grad).view(s)
-            node_lls = node_lls.view(s)
+            s = (
+                module_lls.shape[0],
+                module.out_features,
+                module._in_channels_per_input,
+                module._num_sums,
+                1,
+            )
+            log_grads = torch.log(module_lls.grad).view(s)
+            module_lls = module_lls.view(s)
 
-        log_expectations = log_weights + log_grads + input_lls - node_lls
+        log_expectations = log_weights + log_grads + input_lls - module_lls
         log_expectations = log_expectations.logsumexp(0)  # Sum over batch dimension
         log_expectations = log_expectations.log_softmax(module.sum_dim)  # Normalize
 
@@ -407,5 +485,10 @@ def em(
         module.log_weights = log_expectations
 
         # NOTE: since we explicitely override parameters in 'maximum_likelihood_estimation', we do not need to zero/None parameter gradients
+        # TODO: Check if the above is still true after the whole reimplementation (don't we set param.data = ...?)
 
-    em(module.inputs, data, check_support=check_support, dispatch_ctx=dispatch_ctx)
+    if module.has_single_input:
+        em(module.inputs, data, check_support=check_support, dispatch_ctx=dispatch_ctx)
+    else:
+        for inp in module.inputs:
+            em(inp, data, check_support=check_support, dispatch_ctx=dispatch_ctx)
