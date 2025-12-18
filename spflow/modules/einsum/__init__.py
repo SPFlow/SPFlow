f16f055a8806dc8338a94f8ceb08dfb79c3b2e98
Steven Braun <steven.braun.mz@gmail.com>
2025-12-18 16:52:44 +0100

    feat: Add EinsumNetworks

--- spflow/modules/einsum/__init__.py
+++ spflow/modules/einsum/__init__.py
@@ -4,7 +4,8 @@ Provides the EinsumLayer class combining product and sum operations into
 a single efficient einsum operation for building binary tree structured circuits.
 """
 
+from spflow.modules.einsum.einet import Einet
 from spflow.modules.einsum.einsum_layer import EinsumLayer
 from spflow.modules.einsum.linsum_layer import LinsumLayer
 
-__all__ = ["EinsumLayer", "LinsumLayer"]
+__all__ = ["Einet", "EinsumLayer", "LinsumLayer"]
